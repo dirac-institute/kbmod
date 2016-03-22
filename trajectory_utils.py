9df54dd7f43cc347d14e88ed6ec9ba8336301494
Bryce Kalmbach <jbkalmbach@gmail.com>
2016-03-22 12:38:09 -0700

    Added tools for visualizing trajectories.

---
+++ trajectory_utils.py
@@ -0,0 +1,71 @@
+import numpy as np
+from lsst.sims.utils import CoordinateTransformations as ct
+import matplotlib.pyplot as plt
+
+def inclined_vec(a,i,t,omega0=2*np.pi):
+    omega = a**(-3/2.)*omega0
+    x = a*np.cos(omega*t)
+    y = a*np.cos(i)*np.sin(omega*t)
+    z = a*np.sin(i)*np.sin(omega*t)
+    return x,y,z
+
+def earth_vec(t,omega0=2*np.pi):
+    x = np.cos(omega0*t)
+    y = np.sin(omega0*t)
+    z = 0
+    return x,y,z
+
+def diff_vec(a,i,t,omega0=2*np.pi):
+    x_o, y_o, z_o = inclined_vec(a,i,t,omega0=omega0)
+    x_e, y_e, z_e = earth_vec(t,omega0=omega0)
+    x_new = x_o-x_e
+    y_new = y_o-y_e
+    z_new = z_o-z_e
+    return np.array((x_new, y_new, z_new))
+
+def plot_trajectory(radius, incl, maxTime, dt):
+    """radius, in AU
+       incl, inclination in degrees
+       maxTime, max time of plot in years
+       dt, time step in years
+    """
+    time_step = np.arange(0,maxTime,dt)
+    lon = []
+    lat = []
+    for time in time_step:
+        lon_now,lat_now = ct.sphericalFromCartesian(diff_vec(radius,np.deg2rad(incl),time))
+        lon.append(ct.arcsecFromRadians(lon_now))
+        lat.append(ct.arcsecFromRadians(lat_now))
+    lon = np.array(lon)
+    lat = np.array(lat)
+    #fig = plt.figure(figsize=(12,12))
+    plt.scatter(lon, lat, c=time_step, lw=0)
+    plt.xlabel('Long (arcsec)')
+    plt.ylabel('Lat (arcsec)')
+    plt.title('Trajectory of object over %.2f years' % maxTime)
+    plt.colorbar()
+
+def plot_ang_vel(radius, incl, maxTime, dt):
+    """radius, in AU
+       incl, inclination in degrees
+       maxTime, max time of plot in years
+       dt, time step in years
+    """
+    time_step = np.arange(0,maxTime,dt)
+    lon = []
+    lat = []
+    for time in time_step:
+        lon_now,lat_now = ct.sphericalFromCartesian(diff_vec(radius,np.deg2rad(incl),time))
+        lon.append(lon_now)
+        lat.append(lat_now)
+    lon = np.array(lon)
+    lat = np.array(lat)
+    ang_vel = []
+    for array_val in xrange(0, len(lon)-1):
+        ang_vel.append(ct.arcsecFromRadians(ct.haversine(lon[array_val], lat[array_val],
+                                                         lon[array_val+1], lat[array_val+1]))/(dt*365*24))
+    #fig = plt.figure(figsize=(12,12))
+    plt.plot(time_step[:-1], ang_vel)
+    plt.ylabel('Arcsec/hr')
+    plt.xlabel('Time (yrs)')
+    plt.title('Max Angular Velocity = %.4e arcsec/hr.' % np.max(ang_vel))
