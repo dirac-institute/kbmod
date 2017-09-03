daa4869ba229d6018590cb2f7a48c26cf552d6f2
PWhiddy <all.cows.like.to.moo@gmail.com>
2017-09-03 01:09:40 -0700

    tests for extreme_in_region, sdf, filtering

--- tests/test_dtraj_bounds.py
+++ tests/test_dtraj_bounds.py
@@ -18,7 +18,7 @@ class test_dtraj_bounds(unittest.TestCase):
       self.assertEqual(self.search.biggest_fit(4,4,10,10,32), 4)
       self.assertEqual(self.search.biggest_fit(10,10,50,50,128), 2)
       self.assertEqual(self.search.biggest_fit(10,11,64,64,128), 1)
-      self.assertEqual(self.search.biggest_fit(16,16,64,64,128), 16)      
+      self.assertEqual(self.search.biggest_fit(16,16,64,64,128), 16)
       self.assertEqual(self.search.biggest_fit(16,20,64,64,128), 4)
       self.assertEqual(self.search.biggest_fit(-150,-100,100,125,1024), 2)
       self.assertEqual(self.search.biggest_fit(-64,32,2000,2000,2048), 32)
@@ -29,10 +29,10 @@ class test_dtraj_bounds(unittest.TestCase):
    def test_square_sdf(self):
       self.assertEqual(self.search.square_sdf(1.0, 1.5, 1.5, 1.5, 0.5), 0.5)
       self.assertEqual(self.search.square_sdf(30, 15, 15, 50, 15), 20)
-      self.assertEqual(self.search.square_sdf(1.0, 1.5, 1.5, 1.5, 0.5), 0.5)
-      self.assertEqual(self.search.square_sdf(1.0, 1.5, 1.5, 1.5, 0.5), 0.5)
-      self.assertEqual(self.search.square_sdf(1.0, 1.5, 1.5, 1.5, 0.5), 0.5)
-      self.assertEqual(self.search.square_sdf(1.0, 1.5, 1.5, 1.5, 0.5), 0.5)
+      self.assertEqual(self.search.square_sdf(2.0, 4.5, 2.5, 4.5, 1.5), 0.0)
+      self.assertLess(self.search.square_sdf(10.0, 10.5, 11.5, 12.5, 11.5), 0.0)
+      self.assertEqual(self.search.square_sdf(1.0, 0.0, 0.0, 0.5, 0.5), 0.0)
+      self.assertEqual(self.search.square_sdf(2.0, 6.0, 6.0, 6.0, 6.0), -1.0)
       self.assertEqual(self.search.square_sdf(1.0, 1.5, 1.5, 1.5, 0.5), 0.5)
 
    def test_filter_bounds(self):
@@ -52,18 +52,56 @@ class test_dtraj_bounds(unittest.TestCase):
 
       # filter trajectories out of search bounds
       t_f = self.search.filter_bounds(t_list, 10.0, 10.0, 5.0, 10.0)
-      #self.assertEqual(t_f[0].ix, 50)
-      #self.assertEqual(t_f[0].iy, 50)
-      #self.assertEqual(t_f[0].fx, 100)
-      #self.assertEqual(t_f[0].fy, 100)
-      #self.assertEqual(t_f[1].ix, 280)
-      #self.assertEqual(t_f[1].iy, 130)
-      #self.assertEqual(t_f[1].fx, 330)
-      #self.assertEqual(t_f[1].fy, 180)
-      #self.assertEqual(len(t_f), 2)
+      self.assertEqual(len(t_f), 2)
+      self.assertEqual(t_f[0].ix, 50)
+      self.assertEqual(t_f[0].iy, 50)
+      self.assertEqual(t_f[0].fx, 100)
+      self.assertEqual(t_f[0].fy, 100)
+      self.assertEqual(t_f[1].ix, 280)
+      self.assertEqual(t_f[1].iy, 130)
+      self.assertEqual(t_f[1].fx, 330)
+      self.assertEqual(t_f[1].fy, 180)
+     
+   def test_filter_bounds_depth(self):
+      t_list = []
+      t = kb.dtraj()
+      t.ix, t.iy, t.fx, t.fy, t.depth = 2, 4, 3, 5, 6
+      t_list.append(t)
+      t = kb.dtraj()
+      t.ix, t.iy, t.fx, t.fy, t.depth = 0, 0, 0, 0, 8
+      t_list.append(t)
+      t = kb.dtraj()
+      t.ix, t.iy, t.fx, t.fy, t.depth = 1, 1, -1, -1, 7
+      t_list.append(t)
+      t = kb.dtraj()
+      t.ix, t.iy, t.fx, t.fy, t.depth = -40, 125, 20, 194, 0
+      t_list.append(t)
+      t = kb.dtraj()
+      t.ix, t.iy, t.fx, t.fy, t.depth = 280, 130, 310, 160, 1
+      t_list.append(t)
+      t = kb.dtraj()
+      t.ix, t.iy, t.fx, t.fy, t.depth = 280, 130, 300, 170, 1
+      t_list.append(t)
 
-      # TODO test at high depth levels
-
+      # filter trajectories out of search bounds
+      t_f = self.search.filter_bounds(t_list, 32.0, 32.0, 2.0, 16.0)
+      self.assertEqual(len(t_f), 4)
+      self.assertEqual(t_f[0].ix, 2)
+      self.assertEqual(t_f[0].iy, 4)
+      self.assertEqual(t_f[0].fx, 3)
+      self.assertEqual(t_f[0].fy, 5)
+      self.assertEqual(t_f[1].ix, 0)
+      self.assertEqual(t_f[1].iy, 0)
+      self.assertEqual(t_f[1].fx, 0)
+      self.assertEqual(t_f[1].fy, 0)
+      self.assertEqual(t_f[2].ix, -40)
+      self.assertEqual(t_f[2].iy, 125)
+      self.assertEqual(t_f[2].fx, 20)
+      self.assertEqual(t_f[2].fy, 194)
+      self.assertEqual(t_f[3].ix, 280)
+      self.assertEqual(t_f[3].iy, 130)
+      self.assertEqual(t_f[3].fx, 310)
+      self.assertEqual(t_f[3].fy, 160)
 
 if __name__ == '__main__':
    unittest.main()
--- tests/test_dtraj_regions.py
+++ tests/test_dtraj_regions.py
@@ -5,11 +5,32 @@ class test_dtraj_regions(unittest.TestCase):
 
    def setUp(self):
       # test pass thresholds
-      
-      max_img = kb.raw_image(170, 101)
+      im = kb.layered_image("",171,111,5.0,25.0,0)
+      stack = kb.image_stack([im])
+      p = kb.psf(1.0)
+      self.search = kb.stack_search(stack, p)
+
+      max_img = im.get_science()
+      max_img.set_pixel(38,39,117)
+      max_img.set_pixel(34,53,1000)
+      max_img.set_pixel(50,37, 1000)
+      max_img.set_pixel(70,72, 1000)
       self.pooled_max = []
       while (max_img.get_ppi() > 1):
          self.pooled_max.append(max_img)
          max_img = max_img.pool_max()
-      
 
+      min_img = im.get_science()
+      self.pooled_min = []
+      while (min_img.get_ppi() > 1):
+         self.pooled_min.append(min_img)
+         min_img = min_img.pool_min()
+
+   def test_extreme_in_region(self):
+      self.assertEqual(self.pooled_max[0].get_pixel(38,39), 117)
+      self.assertLess(self.pooled_min[4].get_pixel(2,1), -5.0)
+      self.assertEqual(self.search.extreme_in_region(1.6, 1.7, 32, 
+         self.pooled_max, kb.pool_max), 117)
+
+if __name__ == '__main__':
+   unittest.main()
