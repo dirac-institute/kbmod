013d03a81388d31c7d306b5deb13e849bfe0ae8f
PWhiddy <all.cows.like.to.moo@gmail.com>
2017-10-30 15:56:21 -0700

    simple test template

---
+++ tests/test_import.py
@@ -0,0 +1,13 @@
+import unittest
+from kbmodpy import kbmod as kb
+
+class test_import(unittest.TestCase):
+
+   def setUp(self):
+      #kb.
+      pass
+
+   def test_something(self):
+      #self.assertGreater( a , b )
+      #self.assertEqual( a , b )
+      pass
