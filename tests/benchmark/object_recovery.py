586ea08a8b867daf4fb7a66032160c2018c395bd
PWhiddy <all.cows.like.to.moo@gmail.com>
2017-11-27 01:40:30 -0800

    outline of benchmark test

---
+++ tests/benchmark/object_recovery.py
@@ -0,0 +1,53 @@
+import json
+from kbmodpy import kbmod as kb
+
+def setup_search(path):
+    with open(path, 'r') as fp:
+        params = json.load(fp)
+    t_list = kb.load_trajectories(params['trajectories_file'])
+    psf = kb.psf(params['psf_sigma'])
+    imgs = []
+    for i in range(params['img_count']):
+        time = i/(params['img_count']-1)
+        im = kb.layered_image('img'+str(i), 
+            params['x_dim'],
+            params['y_dim'],
+            params['noise'],
+            params['noise']*params['noise'],
+            time)
+        for t in t_list:
+            im.add_object(t.x+time*t.x_v, t.y+time*t.y_v, t.flux, psf)
+        imgs.append(im)
+
+    stack = kb.image_stack(imgs)
+    del imgs
+    search = kb.stack_search(stack, psf)
+    del stack
+    return search, t_list, params
+
+def grid_search(path):
+    search, t_list, params = setup_search(path)
+    search.gpu( 
+        params['angle_steps'],
+        params['velocity_steps'],
+        params['min_angle'],
+        params['max_angle'],
+        params['min_vel'],
+        params['max_vel'],
+        int(params['img_count']/2))
+    return search.get_results(0, 10000), t_list
+
+def region_search(path):
+    search, t_list, params = setup_search(path)
+    results = search.region_search(
+        params['x_vel'],
+        params['y_vel'],
+        params['radius'],
+        params['min_lh'],
+        int(params['img_count']/2))
+    return kb.region_to_grid(results, 1.0), t_list
+
+def benchmark(path, tp):
+    # do search, print timings
+    # print recovery rates, and overall results score
+    pass
