04a34cfe8890437bf94a5e4fda4e25dea742677b
smotherh <hayden.smotherman@outlook.com>
2018-04-27 10:34:57 -0700

    Added support for multiple python names

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,1 +1,11 @@
-python3 -m unittest
+if hash python3 2>/dev/null; then
+    python3 -m unittest
+elif hash python 2>/dev/null; then
+    if python -c 'import sys; print(sys.version_info[0])' | grep -q 3; then
+        python -m unittest
+    else
+        echo "Python detected, but it must be version 3.x"
+    fi
+else
+    echo "Python not found"
+fi
