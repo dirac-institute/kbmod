e9c0c2bcd943aa89c5ec35d5ec8cab2a4a17fed4
Bryce Kalmbach <jbkalmbach@gmail.com>
2019-07-25 12:50:01 -0700

    Added explanations to notebook and renamed.

--- setup.bash
+++ setup.bash
@@ -1,12 +1,13 @@
 export PYTHONPATH=$PWD/analysis:$PWD/search/pybinds:$PYTHONPATH
+
 # Disable python multiprocessing
 export OPENBLAS_NUM_THREADS=1
 export MKL_NUM_THREADS="N"
 export MKL_DYNAMIC="FALSE"
 export OMP_NUM_THREADS=1
-#setup pyephem
 
-export PYORBFIT_HOME=/phys/users/brycek/epyc/users/brycek/pyOrbfit
+# Set up pyOrbfit for Orbit Fitting
+export PYORBFIT_HOME=/phys/users/brycek/epyc/users/brycek/dirac-pyOrbfit
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PYORBFIT_HOME
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:$PYORBFIT_HOME
