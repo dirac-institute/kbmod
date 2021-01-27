c9129d4c15f305201f4dbc30adb76facc34291af
Hayden Smotherman <hayden.smotherman@outlook.com>
2021-01-27 15:47:04 -0800

    Added more visualization classes and functions

--- setup.bash
+++ setup.bash
@@ -7,7 +7,7 @@ export MKL_DYNAMIC="FALSE"
 export OMP_NUM_THREADS=1
 
 # Set up pyOrbfit for Orbit Fitting
-export PYORBFIT_HOME=/phys/users/brycek/epyc/users/brycek/dirac-pyOrbfit
+export PYORBFIT_HOME=/astro/store/epyc/users/brycek/dirac-pyOrbfit
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PYORBFIT_HOME
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:$PYORBFIT_HOME
