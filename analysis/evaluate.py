e184acba09d26de0413a65edc058f9b4794a648f
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2022-09-19 10:58:08 -0400

    Add a medium sized test
    Add a more comprehensive end-to-end regression test and some evaluation functions to help with future correctness testing.

---
+++ analysis/evaluate.py
@@ -0,0 +1,157 @@
+import math
+from kbmod import *
+
+def trajectory_distances(trjA, trjB, times=[0.0]):
+    """
+    Evaluate the distance between two trajectories (in pixels)
+    at different times.
+    
+    Arguments:
+        trjA : trajectory
+            The first trajectory to evaluate.
+        trjB : trajectory
+            The second trajectory to evaluate.
+        times : list
+            The list of zero-shifted times at which to evaluate the
+            matches. The average of the distances at these times
+            are used.
+
+    Returns:
+        list : The distance at each time.
+    """
+    num_times = len(times)
+    results = [0.0] * num_times
+    for i in range(num_times):
+        xA = trjA.x + trjA.x_v * times[i]
+        yA = trjA.y + trjA.y_v * times[i]
+        xB = trjB.x + trjB.x_v * times[i]
+        yB = trjB.y + trjB.y_v * times[i]
+        results[i] = math.sqrt((xA - xB)**2 + (yA - yB)**2)
+    return results
+
+
+def ave_trajectory_distance(trjA, trjB, times=[0.0]):
+    """
+    Evaluate the average distance between two trajectories (in pixels)
+    at different times.
+    
+    Arguments:
+        trjA : trajectory
+            The first trajectory to evaluate.
+        trjB : trajectory
+            The second trajectory to evaluate.
+        times : list
+            The list of zero-shifted times at which to evaluate the
+            matches. The average of the distances at these times
+            are used.
+
+    Returns:
+        float : The average distance in pixels.
+    """
+    num_times = len(times)
+    assert(num_times > 0)
+
+    dists = trajectory_distances(trjA, trjB, times)
+
+    sum = 0.0
+    for d in dists:
+        sum += d
+    return sum / float(num_times)
+
+
+def find_unique_overlap(traj_query, traj_base, threshold, times=[0.0]):
+    """
+    Finds the set of trajectories in traj_query that are 'close' to
+    trajectories in traj_base such that each trajectory in traj_base
+    is used at most once.
+
+    Used to evaluate the performance of algorithms.
+
+    Arguments:
+        traj1 : list
+            A list of trajectories to compare.
+        traj2 : list
+            The second list of trajectories to compare.
+        threshold : float
+            The distance threshold between two observations
+            to count a match (in pixels).
+        times : list
+            The list of zero-shifted times at which to evaluate the
+            matches. The average of the distances at these times
+            are used.
+
+    Returns:
+        int : The number of objects that appear in both traj1 and traj2
+              where each trajectory in each set is only used once.
+    """
+    num_times = len(times)
+    size_base = len(traj_base)
+    used = [False] * size_base
+
+    results = []
+    for query in traj_query:
+        best_dist = 10.0 * threshold
+        best_ind = -1
+
+        # Check the current query against all unused base trajectories.
+        for j in range(size_base):
+            if not used[j]:
+                dist = ave_trajectory_distance(query, traj_base[j], times)
+                if dist < best_dist:
+                    best_dist = dist
+                    best_ind = j
+        
+        # If we found a good match, save it.
+        if best_dist <= threshold:
+            results.append(query)
+            used[best_ind] = True
+    return results
+
+def find_set_difference(traj_query, traj_base, threshold, times=[0.0]):
+    """
+    Finds the set of trajectories in traj_query that are NOT 'close' to
+    any trajectories in traj_base such that each trajectory in traj_base
+    is used at most once.
+
+    Used to evaluate the performance of algorithms.
+
+    Arguments:
+        traj_query : list
+            A list of trajectories to compare.
+        traj_base : list
+            The second list of trajectories to compare.
+        threshold : float
+            The distance threshold between two observations
+            to count a match (in pixels).
+        times : list
+            The list of zero-shifted times at which to evaluate the
+            matches. The average of the distances at these times
+            are used.
+
+    Returns:
+        int : The number of objects that appear in both traj1 and traj2
+              where each trajectory in each set is only used once.
+    """
+    num_times = len(times)
+    size_base = len(traj_base)
+    used = [False] * size_base
+
+    results = []
+    for query in traj_query:
+        best_dist = 10.0 * threshold
+        best_ind = -1
+
+        # Check the current query against all unused base trajectories.
+        for j in range(size_base):
+            if not used[j]:
+                dist = ave_trajectory_distance(query, traj_base[j], times)
+                if dist < best_dist:
+                    best_dist = dist
+                    best_ind = j
+        
+        # If we found a good match, save it.
+        if best_dist <= threshold:
+            used[best_ind] = True
+        else:
+            results.append(query)
+    return results
