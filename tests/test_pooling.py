e035a95d6cefacdc009fc0f11bd4c2a6de1055fa
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2022-10-18 16:23:47 -0400

    Clean up test_pooling.py

--- tests/test_pooling.py
+++ tests/test_pooling.py
@@ -57,41 +57,28 @@ class test_pooling(unittest.TestCase):
     def test_pooling_to_one(self):
         depth = 10
         res = 2**depth
-        im = kb.layered_image("test", res, res, 0.0, 1.0, 0.0, self.p)
-        im = im.get_science()
-        for _ in range(depth):
-            im = im.pool_max()
-        im = np.array(im)
-        self.assertEqual(im[0][0], 0.0)
-
-        im = kb.layered_image("test", res, res, 0.0, 1.0, 0.0, self.p)
-        im = im.get_science()
-        for _ in range(depth):
-            im = im.pool_min()
-        im = np.array(im)
-        self.assertEqual(im[0][0], 0.0)
-
         im = kb.layered_image("test", res, res, 3.0, 9.0, 0.0, self.p)
         im = im.get_science()
         test_high = 142.6
         test_low = -302.2
         im.set_pixel(51, 55, test_high)
         im.set_pixel(20, 18, test_low)
+
         # reduce to max
-        imax = im.pool_max()
+        imax = im.pool_max(False)
         for _ in range(depth - 1):
-            imax = imax.pool_max()
+            imax = imax.pool_max(False)
         imax = np.array(imax)
         self.assertAlmostEqual(imax[0][0], test_high, delta=0.001)
 
         # reduce to min
-        imin = im.pool_min()
+        imin = im.pool_min(False)
         for _ in range(depth):
-            imin = imin.pool_min()
+            imin = imin.pool_min(False)
         imin = np.array(imin)
         self.assertAlmostEqual(imin[0][0], test_low, delta=0.001)
 
-     def test_pooling_max_symmetric_small(self):
+    def test_pooling_max_symmetric_small(self):
         """
         Tests two-sided max pooling on a manually constructed 4 x 4 example.
         """
@@ -137,13 +124,12 @@ class test_pooling(unittest.TestCase):
         self.assertAlmostEqual(pooled.get_pixel(1, 0), 1.0, delta=1e-8)
         self.assertAlmostEqual(pooled.get_pixel(1, 1), 0.5, delta=1e-8)
 
-
     def test_all_pix(self):
         im = kb.raw_image(100, 100)
         test_val = 402.0
         im.set_all(test_val)
         for _ in range(8):
-            im = im.pool_max()
+            im = im.pool_max(False)
         self.assertAlmostEqual(np.array(im)[0][0], test_val, delta=0.001)
 
     def test_pool_in_place(self):
