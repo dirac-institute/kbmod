5c2f19bdb0302d8e2b5719ec5ad6cf717ff4cf96
DinoBektesevic <dinob@uw.edu>
2022-11-03 10:46:35 -0700

    Resolve review comments.

--- src/kbmod/analysis/__init__.py
+++ src/kbmod/analysis/__init__.py
@@ -1,22 +1,2 @@
-# we skip all imports because of the large 
-# external libraries they load and nobody
-# seems to be using these scripts atm 
-
-#from . import evaluate
-#from . import create_stamps
-#from . import jointfit_functions
-#from . import known_objects
-#from . import precovery_utils
-
-# We do not import this because pyOrbfit,
-# even when correctly setup-ed doesn't seem to
-# contain pyOrbfit.Orbit
-#from . import orbit_utils
-
-# We do not import his because it requires
-# the entire LSST stack to be installed
-#from . import trajectory_utils
-#    from . import trajectory_utils
-#  File "/astro/store/epycn/users/dinob/kbmod/build_system/kbmodpy/src/kbmodpy/trajectory_utils.py", line 3, in <module>
-#    from lsst.sims.utils import CoordinateTransformations as ct
-#ModuleNotFoundError: No module named 'lsst'
+# Do not import modules here.
+# Some of the modules have dependencies that are optional.
