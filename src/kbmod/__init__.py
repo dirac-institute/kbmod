5389820b8f6e27528bcec80b2831c4224075f33e
DinoBektesevic <dinob@uw.edu>
2022-11-02 18:58:24 -0700

    Write version to file, otherwise import takes too long. Cleanup.

--- src/kbmod/__init__.py
+++ src/kbmod/__init__.py
@@ -1,3 +1,13 @@
+import warnings 
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    warnings.warn(
+        "Unable to determine the package version. "
+        "This is likely a broken installation."
+    )
+
 from . import kbmodpy
 from . import run_search
 from . import image_info
