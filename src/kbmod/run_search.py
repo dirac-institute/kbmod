23fa67356f1ec62f063d055648717ead491ed063
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-02-06 16:44:21 -0500

    Move the astro helpers into run_search.py

--- src/kbmod/run_search.py
+++ src/kbmod/run_search.py
@@ -2,13 +2,16 @@ import time
 import warnings
 import os
 
+from astropy.time import Time
+import astropy.units as u
+import astropy.coordinates as astroCoords
 import numpy as np
 from .analysis_utils import Interface, PostProcess
-from .astro_utils import *
 from .image_info import *
 import kbmod.search as kb
 import koffi
 from .result_list import *
+from numpy.linalg import lstsq
 
 
 class run_search:
@@ -117,7 +120,7 @@ class run_search:
             raise ValueError("Results filepath not set")
         return
 
-    def do_gpu_search(self, search, img_info, ec_angle, post_process):
+    def do_gpu_search(self, search, img_info, suggested_angle, post_process):
         """
         Performs search on the GPU.
 
@@ -127,7 +130,7 @@ class run_search:
             Search object.
         img_info : `kbmod.search.ImageInfo`
             ImageInfo object.
-        ec_angle : `float`
+        suggested_angle : `float`
             Angle a 12 arcsecond segment parallel to the ecliptic is
             seen under from the image origin.
         post_process :
@@ -138,7 +141,7 @@ class run_search:
         # Run the grid search
         # Set min and max values for angle and velocity
         if self.config["average_angle"] == None:
-            average_angle = ec_angle
+            average_angle = suggested_angle
         else:
             average_angle = self.config["average_angle"]
         ang_min = average_angle - self.config["ang_arr"][0]
@@ -151,7 +154,7 @@ class run_search:
         # If we are using barycentric corrections, compute the parameters and
         # enable it in the search function.
         if "bary_dist" in self.config.keys() and self.config["bary_dist"] is not None:
-            bary_corr = calc_barycentric_corr(img_info, self.config["bary_dist"])
+            bary_corr = self._calc_barycentric_corr(img_info, self.config["bary_dist"])
             # print average barycentric velocity for debugging
 
             mjd_range = img_info.get_duration()
@@ -172,7 +175,7 @@ class run_search:
             "Min Velocity",
             "Max Velocity",
         )
-        param_values = (ec_angle, *search_params["ang_lims"], *search_params["vel_lims"])
+        param_values = (suggested_angle, *search_params["ang_lims"], *search_params["vel_lims"])
         for header, val in zip(param_headers, param_values):
             print("%s = %.4f" % (header, val))
 
@@ -250,7 +253,7 @@ class run_search:
 
         # Compute the ecliptic angle for the images.
         center_pixel = (img_info.stats[0].width / 2, img_info.stats[0].height / 2)
-        ec_angle = calc_ecliptic_angle(img_info.stats[0].wcs, center_pixel)
+        suggested_angle = self._calc_suggested_angle(img_info.stats[0].wcs, center_pixel)
 
         # Set up the post processing data structure.
         kb_post_process = PostProcess(self.config, img_info.get_all_mjd())
@@ -266,7 +269,7 @@ class run_search:
 
         # Perform the actual search.
         search = kb.stack_search(stack)
-        search, search_params = self.do_gpu_search(search, img_info, ec_angle, kb_post_process)
+        search, search_params = self.do_gpu_search(search, img_info, suggested_angle, kb_post_process)
 
         # Load the KBMOD results into Python and apply a filter based on
         # 'filter_type.
@@ -381,3 +384,133 @@ class run_search:
         if num_found > 0:
             print(matches_string)
         print("-----------------")
+
+    def _calc_barycentric_corr(self, img_info, dist):
+        """
+        This function calculates the barycentric corrections between
+        each image and the first.
+
+        The barycentric correction is the shift in x,y pixel position expected for
+        an object that is stationary in barycentric coordinates, at a barycentric
+        radius of dist au. This function returns a linear fit to the barycentric
+        correction as a function of position on the first image.
+
+        Parameters
+        ----------
+        img_info : `kbmod.search.ImageInfo`
+            ImageInfo
+        dist : `float`
+            Distance to object from barycenter in AU.
+        """
+        from astropy import units as u
+        from astropy.coordinates import SkyCoord, get_body_barycentric, solar_system_ephemeris
+        from astropy.time import Time
+        from numpy.linalg import lstsq
+
+        wcslist = [img_info.stats[i].wcs for i in range(img_info.num_images)]
+        mjdlist = np.array(img_info.get_all_mjd())
+        x_size = img_info.get_x_size()
+        y_size = img_info.get_y_size()
+
+        # make grid with observer-centric RA/DEC of first image
+        xlist, ylist = np.mgrid[0:x_size, 0:y_size]
+        xlist = xlist.flatten()
+        ylist = ylist.flatten()
+        cobs = wcslist[0].pixel_to_world(xlist, ylist)
+
+        # convert this grid to barycentric x,y,z, assuming distance r
+        # [obs_to_bary_wdist()]
+        with solar_system_ephemeris.set("de432s"):
+            obs_pos = get_body_barycentric("earth", Time(mjdlist[0], format="mjd"))
+        cobs.representation_type = "cartesian"
+        # barycentric distance of observer
+        r2_obs = obs_pos.x * obs_pos.x + obs_pos.y * obs_pos.y + obs_pos.z * obs_pos.z
+        # calculate distance r along line of sight that gives correct
+        # barycentric distance
+        # |obs_pos + r * cobs|^2 = dist^2
+        # obs_pos^2 + 2r (obs_pos dot cobs) + cobs^2 = dist^2
+        dot = obs_pos.x * cobs.x + obs_pos.y * cobs.y + obs_pos.z * cobs.z
+        bary_dist = dist * u.au
+        r = -dot + np.sqrt(bary_dist * bary_dist - r2_obs + dot * dot)
+        # barycentric coordinate is observer position + r * line of sight
+        cbary = SkyCoord(
+            obs_pos.x + r * cobs.x,
+            obs_pos.y + r * cobs.y,
+            obs_pos.z + r * cobs.z,
+            representation_type="cartesian",
+        )
+
+        baryCoeff = np.zeros((len(wcslist), 6))
+        for i in range(1, len(wcslist)):  # corections for wcslist[0] are 0
+            # hold the barycentric coordinates constant and convert to new frame
+            # by subtracting the observer's new position and converting to RA/DEC and pixel
+            # [bary_to_obs_fast()]
+            with solar_system_ephemeris.set("de432s"):
+                obs_pos = get_body_barycentric("earth", Time(mjdlist[i], format="mjd"))
+            c = SkyCoord(
+                cbary.x - obs_pos.x, cbary.y - obs_pos.y, cbary.z - obs_pos.z, representation_type="cartesian"
+            )
+            c.representation_type = "spherical"
+            pix = wcslist[i].world_to_pixel(c)
+
+            # do linear fit to get coefficients
+            ones = np.ones_like(xlist)
+            A = np.stack([ones, xlist, ylist], axis=-1)
+            coef_x, _, _, _ = lstsq(A, (pix[0] - xlist))
+            coef_y, _, _, _ = lstsq(A, (pix[1] - ylist))
+            baryCoeff[i, 0:3] = coef_x
+            baryCoeff[i, 3:6] = coef_y
+
+        return baryCoeff
+        
+    def _calc_suggested_angle(self, wcs, center_pixel=(1000, 2000), step=12):
+        """Projects an unit-vector parallel with the ecliptic onto the image
+        and calculates the angle of the projected unit-vector in the pixel
+        space.
+
+        Parameters
+        ----------
+        wcs : `astropy.wcs.WCS`
+            World Coordinate System object.
+        center_pixel : tuple, array-like
+            Pixel coordinates of image center.
+        step : float or int
+            Size of step, in arcseconds, used to find the pixel coordinates of
+                the second pixel in the image parallel to the ecliptic.
+
+        Returns
+        -------
+        suggested_angle : float
+            Angle the projected unit-vector parallel to the ecliptic
+            closes with the image axes. Used to transform the specified
+            search angles, with respect to the ecliptic, to search angles
+            within the image.
+
+        Note
+        ----
+        It is not neccessary to calculate this angle for each image in an
+        image set if they have all been warped to a common WCS.
+
+        See Also
+        --------
+        run_search.do_gpu_search
+        """
+        # pick a starting pixel approximately near the center of the image
+        # convert it to ecliptic coordinates
+        start_pixel = np.array(center_pixel)
+        start_pixel_coord = astroCoords.SkyCoord.from_pixel(start_pixel[0], start_pixel[1], wcs)
+        start_ecliptic_coord = start_pixel_coord.geocentrictrueecliptic
+
+        # pick a guess pixel by moving parallel to the ecliptic
+        # convert it to pixel coordinates for the given WCS
+        guess_ecliptic_coord = astroCoords.SkyCoord(
+            start_ecliptic_coord.lon + step * u.arcsec,
+            start_ecliptic_coord.lat,
+            frame="geocentrictrueecliptic",
+        )
+        guess_pixel_coord = guess_ecliptic_coord.to_pixel(wcs)
+
+        # calculate the distance, in pixel coordinates, between the guess and
+        # the start pixel. Calculate the angle that represents in the image.
+        x_dist, y_dist = np.array(guess_pixel_coord) - start_pixel
+        return np.arctan2(y_dist, x_dist)
