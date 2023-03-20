7298d084260b012ce9444e4fa71b21695296c2e1
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-03-20 13:59:15 -0400

    Add comments requested in PR

--- src/kbmod/filters/base_filter.py
+++ src/kbmod/filters/base_filter.py
@@ -11,8 +11,28 @@ class Filter(abc.ABC):
 
     @abc.abstractmethod
     def get_filter_name(self):
+        """Get the name of the filter.
+
+        Returns
+        -------
+        str
+            The filter name.
+        """
         pass
 
     @abc.abstractmethod
     def keep_row(self, row: ResultRow):
+        """Determine whether to keep an individual row based on
+        the row's information and the parameters of the filter.
+
+        Parameters
+        ----------
+        row : ResultRow
+            The row to evaluate.
+
+        Returns
+        -------
+        bool
+           An indicator of whether to keep the row.
+        """
         pass
