fd7ec5b7e3ad9282f6221866fab1640f4020cae6
cchris28 <cchris28@uw.edu>
2023-05-01 08:49:42 -0700

    benchmark for calc_barycentric_corr (#314)
    * initial benchmark.py
    
    * black and isort
    
    * added usage for compare
    
    * ran isort

---
+++ tests/benchmark.py
@@ -0,0 +1,48 @@
+"""Benchmark the barycentric correction
+
+This script benchmarks the barycentric correction function of run_search.
+Usage: python -m pytest tests/benchmark.py
+       python -m pytest tests/benchmark.py --benchmark-autosave --benchmark-compare=0001
+"""
+import time
+
+import pytest
+
+import kbmod
+from kbmod.configuration import KBMODConfig
+
+im_filepath = "../data/demo"
+v_arr = [0, 20, 21]
+ang_arr = [0.5, 0.5, 11]
+
+input_parameters = {
+    # Required
+    "im_filepath": im_filepath,
+    "res_filepath": None,
+    "time_file": None,
+    "output_suffix": "DEMO",
+    "v_arr": v_arr,
+    "ang_arr": ang_arr,
+    "bary_dist": 50.0,
+}
+
+
+def test_benchmark(benchmark):
+    """Benchmark the barycentric correction"""
+    # Test the calc_barycentric function of run_search
+    run_search = kbmod.run_search.run_search(input_parameters)
+    # Load the PSF.
+    kb_interface = kbmod.analysis_utils.Interface()
+    default_psf = kbmod.search.psf(run_search.config["psf_val"])
+
+    # Load images to search
+    _, img_info = kb_interface.load_images(
+        run_search.config["im_filepath"],
+        run_search.config["time_file"],
+        run_search.config["psf_file"],
+        run_search.config["mjd_lims"],
+        default_psf,
+        verbose=run_search.config["debug"],
+    )
+    baryCoeff = benchmark(run_search._calc_barycentric_corr, img_info, 50.0)
+    assert baryCoeff is not None
