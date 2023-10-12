d9f3300eb602a3b1d43cddfe7bdbae72376f42fe
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-10-12 10:58:18 -0400

    Update debug_timer.h

--- src/kbmod/search/debug_timer.h
+++ src/kbmod/search/debug_timer.h
@@ -1,6 +1,8 @@
 /* A very simple class that wraps debug output with timing. Used for
    providing status updates as the system is running if in verbose
    mode and nothing otherwise.
+
+   This is *not* a high precision timer meant to be used for benchmarking.
 */
 #ifndef DEBUG_TIMER_H_
 #define DEBUG_TIMER_H_
@@ -27,8 +29,8 @@ public:
     void stop() {
         if (verbose_) {
             std::chrono::time_point<std::chrono::system_clock> t_end = std::chrono::system_clock::now();
-            auto t_delta = std::chrono::duration_cast<std::chrono::seconds>(t_end - t_start_);
-            std::cout << name_ << " took " << t_delta.count() << " seconds." << std::flush;
+            auto t_delta = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start_);
+            std::cout << name_ << " took " << t_delta.count() / 1000.0 << " seconds.\n" << std::flush;
         }
     }
 
