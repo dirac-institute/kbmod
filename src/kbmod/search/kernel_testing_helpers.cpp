1ad53081e998cae2342f3f67c70e8e6da0f2604d
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-12-15 11:17:46 -0500

    Simplify testing function

--- src/kbmod/search/kernel_testing_helpers.cpp
+++ src/kbmod/search/kernel_testing_helpers.cpp
@@ -14,26 +14,19 @@ extern "C" void SigmaGFilteredIndicesCU(float *values, int num_values, float sgl
  * Return the list of indices from the values array such that those elements
  * pass the sigmaG filtering defined by percentiles [sgl0, sgl1] with coefficient
  * sigma_g_coeff and a multiplicative factor of width.
+ *
+ * The vector values is passed by value to create a local copy which will be modified by
+ * SigmaGFilteredIndicesCU.
  */
-std::vector<int> sigmaGFilteredIndices(const std::vector<float> &values, float sgl0, float sgl1,
-                                       float sigma_g_coeff, float width) {
-    // Bounds check the percentile values.
-    assert(sgl0 > 0.0);
-    assert(sgl1 < 1.0);
-
-    // Allocate space for the input and result.
-    const int num_values = values.size();
-    float values_arr[num_values];
-    int idx_array[num_values];
-    for (int i = 0; i < num_values; ++i) {
-        values_arr[i] = values[i];
-    }
-
+std::vector<int> sigmaGFilteredIndices(std::vector<float> values, float sgl0, float sgl1, float sigma_g_coeff,
+                                       float width) {
+    int num_values = values.size();
+    std::vector<int> idx_array(num_values, 0);
     int min_keep_idx = 0;
     int max_keep_idx = num_values - 1;
 
 #ifdef HAVE_CUDA
-    SigmaGFilteredIndicesCU(values_arr, num_values, sgl0, sgl1, sigma_g_coeff, width, idx_array,
+    SigmaGFilteredIndicesCU(values.data(), num_values, sgl0, sgl1, sigma_g_coeff, width, idx_array.data(),
                             &min_keep_idx, &max_keep_idx);
 #else
     throw std::runtime_error("Non-GPU SigmaGFilteredIndicesCU is not implemented.");
