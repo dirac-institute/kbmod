497e044b10cc2b7a2ca2082dab8da0ddcb6d36ef
DinoBektesevic <dinob@uw.edu>
2024-01-17 13:18:09 -0800

    Initial restructuring of the repository into a flat layout.

--- setup.py
+++ setup.py
@@ -8,14 +8,6 @@ from packaging.version import Version
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-# Convert distutils Windows platform specifiers to CMake -A arguments
-PLAT_TO_CMAKE = {
-    "win32": "Win32",
-    "win-amd64": "x64",
-    "win-arm32": "ARM",
-    "win-arm64": "ARM64",
-}
-
 
 # A CMakeExtension needs a sourcedir instead of a file list.
 # The name must be the _single_ output extension from the CMake build.
@@ -27,6 +19,8 @@ class CMakeExtension(Extension):
 
 
 class CMakeBuild(build_ext):
+    # helper function to execute cmd --version in terminal
+    # and check against minimal allowed version
     def try_cmake(self, cmakecmd, min_version="3.5.0"):
         try:
             out = subprocess.check_output([cmakecmd, '--version'])
@@ -40,6 +34,8 @@ class CMakeBuild(build_ext):
 
         return True
 
+    # execute cmake and then cmake3 commands in the terminal
+    # to find which one provides the minimum required version
     def set_cmake(self):
         if self.try_cmake("cmake"):
             self.cmake = "cmake"
@@ -58,70 +54,30 @@ class CMakeBuild(build_ext):
         ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)  # type: ignore[no-untyped-call]
         extdir = ext_fullpath.parent.resolve()
 
-        # Using this requires trailing slash for auto-detection & inclusion of
-        # auxiliary "native" libs
-
+        ###
+        # Set compilation flags
+        ###
+        # Check for compilation mode flags in the environment
         debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
         cfg = "Debug" if debug else "Release"
 
-        # CMake lets you override the generator - we need to check this.
-        # Can be set with Conda-Build, for example.
-        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
-
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # Set various CMAKE compilation flags, e.g. here we add Python_EXECUTABLE
+        # required for PYBIND11_FINDPYTHON discovry
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
-            f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
+            f"-DCMAKE_BUILD_TYPE={cfg}",
             f"-DPYBIND11_FINDPYTHON=ON",
         ]
-        build_args = []
-        # Adding CMake arguments set as environment variable
-        # (needed e.g. to build for ARM OSx on conda-forge)
+
+        # Add the setuptools-scm version to CMAKE flags
+        cmake_args += [f"-DSDISTVERSION={self.distribution.get_version()}"]  # type: ignore[attr-defined]
+
+        # Add additional CMake arguments found in environment, if any
         if "CMAKE_ARGS" in os.environ:
             cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
 
-        # Provide a version as determined by setuptools-scm
-        cmake_args += [f"-DSDISTVERSION={self.distribution.get_version()}"]  # type: ignore[attr-defined]
-
-        if self.compiler.compiler_type != "msvc":
-            # Using Ninja-build since it a) is available as a wheel and b)
-            # multithreads automatically. MSVC would require all variables be
-            # exported for Ninja to pick it up, which is a little tricky to do.
-            # Users can override the generator with CMAKE_GENERATOR in CMake
-            # 3.15+.
-            if not cmake_generator or cmake_generator == "Ninja":
-                try:
-                    import ninja  # noqa: F401
-
-                    ninja_executable_path = Path(ninja.BIN_DIR) / "ninja"
-                    cmake_args += [
-                        "-GNinja",
-                        f"-DCMAKE_MAKE_PROGRAM:FILEPATH={ninja_executable_path}",
-                    ]
-                except ImportError:
-                    pass
-
-        else:
-            # Single config generators are handled "normally"
-            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
-
-            # CMake allows an arch-in-generator style for backward compatibility
-            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
-
-            # Specify the arch if using MSVC generator, but only if it doesn't
-            # contain a backward-compatibility arch spec already in the
-            # generator name.
-            if not single_config and not contains_arch:
-                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
-
-            # Multi-config generators have a different way to specify configs
-            if not single_config:
-                cmake_args += [
-                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
-                ]
-                build_args += ["--config", cfg]
-
+        # Cross-compile support for macOS - respect ARCHFLAGS if set
         if sys.platform.startswith("darwin"):
             # Cross-compile support for macOS - respect ARCHFLAGS if set
             archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
@@ -136,8 +92,11 @@ class CMakeBuild(build_ext):
             cmake_args += ["-DCPU_ONLY=ON"]
             print("WARNING: No GPU Found. Building with CPU only mode.")
 
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
+        ###
+        # Set build execution flags
+        ###
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control N threads used to build
+        build_args = []
         if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
             # self.parallel is a Python 3 only way to set parallel jobs by hand
             # using -j in the build_ext call, not supported by pip or PyPA-build.
@@ -145,10 +104,14 @@ class CMakeBuild(build_ext):
                 # CMake 3.12+ only.
                 build_args += [f"-j{self.parallel}"]
 
+        # build in temporary directories, so create them
         build_temp = Path(self.build_temp) / ext.name
         if not build_temp.exists():
             build_temp.mkdir(parents=True)
 
+        ###
+        # Run the build
+        ###
         subprocess.run(
             [self.cmake, ext.sourcedir] + cmake_args, cwd=build_temp, check=True
         )
@@ -157,8 +120,6 @@ class CMakeBuild(build_ext):
         )
 
 
-# The information here can also be placed in setup.cfg - better separation of
-# logic and declaration, and simpler if you include description/version in a file.
 setup(
     ext_modules=[CMakeExtension("kbmod.search")],
     cmdclass={"build_ext": CMakeBuild},
