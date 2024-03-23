c60249d211be679b84c9bb2d7cce4826dcf1dde4
DinoBektesevic <dinob@uw.edu>
2024-03-22 21:11:23 -0700

    Add better data mocking, realistic and simplicit data factories, add object injection.

--- src/kbmod/mocking/fits.py
+++ src/kbmod/mocking/fits.py
@@ -1,29 +1,29 @@
 import types
 import functools
 
+import numpy as np
 from astropy.wcs import WCS
-from astropy.io.fits import (
-    HDUList,
-    PrimaryHDU,
-    CompImageHDU,
-    BinTableHDU,
-    Header
-)
+from astropy.modeling import models
+from astropy.io.fits import HDUList, PrimaryHDU, CompImageHDU, BinTableHDU, Header
 
 from .headers import StaticHeader, MutableHeader
-from .fits_data import ZeroedData, SimpleMask
+from .src_catalogs import (
+    gen_random_static_source_catalog,
+    gen_random_moving_object_catalog
+)
+from .fits_data import (
+    ZeroedData,
+    SimpleImage,
+    SimpleVariance,
+    SimpleMask,
+    add_model_objects
+)
 
 
-__all__ = [
-    "HDUFactory",
-    "HDUListFactory",
-    "callback",
-    "SimpleFits",
-    "DECamImdiffs"
-]
+__all__ = ["HDUFactory", "HDUListFactory", "callback", "SimpleFits", "DECamImdiffs"]
 
 
-class HDUFactory():
+class HDUFactory:
     def __init__(self, hdu_cls, header_factory=None, data_factory=None):
         self.hdu_cls = hdu_cls
 
@@ -44,12 +44,12 @@ class HDUFactory():
             data = self.data_factory.mock(hdu=hdu, **kwargs)
             hdu.data = data
             # not sure if we want this tbh
-            #hdu.update_header()
+            # hdu.update_header()
 
         return hdu
 
 
-class HDUListFactory():
+class HDUListFactory:
     def __init__(self, layout, base_primary={}, base_ext={}, base_wcs={}):
         self.layout = layout
         self.base_primary = base_primary
@@ -70,10 +70,10 @@ class HDUListFactory():
             header.update(self.gen_wcs())
         return header
 
-    def gen_primary(self, metadata=None, extend_base=True, add_wcs=False):
+    def gen_primary_hdr(self, metadata=None, extend_base=True, add_wcs=False):
         return self.gen_header(self.base_primary, metadata, extend_base, add_wcs)
 
-    def gen_ext(self, metadata=None, extend_base=True, add_wcs=True):
+    def gen_ext_hdr(self, metadata=None, extend_base=True, add_wcs=True):
         return self.gen_header(self.base_ext, metadata, extend_base, add_wcs)
 
     def mock(self, **kwargs):
@@ -93,7 +93,7 @@ def callback(func):
             @functools.wraps(func)
             def f(*fargs, **fkwargs):
                 kwargs.update(fkwargs)
-                return func(*(args+fargs), **kwargs)
+                return func(*(args + fargs), **kwargs)
             return f
     else:
         # functions, static methods
@@ -106,7 +106,7 @@ def callback(func):
     return wrapper
 
 
-class SimpleFits(HDUListFactory):
+class EmptyFits(HDUListFactory):
     base_primary = {
         "EXTNAME": "PRIMARY",
         "NAXIS": 0,
@@ -115,43 +115,39 @@ class SimpleFits(HDUListFactory):
         "NEXTEND": 3,
         "OBS-LAT": -30.166,
         "OBS-LONG": -70.814,
-        "STD": "SimpleFits"
+        "STD": "SimpleFits",
     }
 
-    base_ext = {
-        "NAXIS": 2,
-        "NAXIS1": 2048,
-        "NAXIS2": 4096,
-        "BITPIX": 32
-    }
+    base_ext = {"NAXIS": 2, "NAXIS1": 2048, "NAXIS2": 4096, "BITPIX": 32}
 
     base_wcs = {
         "crpix": [1024.0, 2048.0],
-        "crval" : [351, -5],
+        "crval": [351, -5],
         "ctype": ["RA---TAN", "DEC--TAN"],
         "cunit": ["deg", "deg"],
         "radesys": "ICRS",
-        "cd": [
-            [-1.44e-07, 7.32e-05],
-            [7.32e-05, 1.44e-05]
-        ]
+        "cd": [[-1.44e-07, 7.32e-05], [7.32e-05, 1.44e-05]],
     }
 
     @callback
     @staticmethod
     def increment_obstime(old, dt):
-        return old+dt
+        return old + dt
 
     def __init__(self):
         primary_hdr = self.gen_primary()
 
         # multiple options on how to handle callbacks as class members
-        #callbacks=[lambda old: old+0.01, ]
-        #callbacks=[functools.partial(self.increment_obstime, dt=0.001), ]
+        # callbacks=[lambda old: old+0.01, ]
+        # callbacks=[functools.partial(self.increment_obstime, dt=0.001), ]
         primary_header_factory = MutableHeader(
             metadata=primary_hdr,
-            mutables=["OBS-MJD", ],
-            callbacks=[self.increment_obstime(dt=0.001), ]
+            mutables=[
+                "OBS-MJD",
+            ],
+            callbacks=[
+                self.increment_obstime(dt=0.001),
+            ],
         )
 
         image_hdr = self.gen_ext({"EXTNAME": "IMAGE"}, add_wcs=True)
@@ -164,10 +160,155 @@ class SimpleFits(HDUListFactory):
             HDUFactory(PrimaryHDU, primary_header_factory),
             HDUFactory(CompImageHDU, StaticHeader(image_hdr), data),
             HDUFactory(CompImageHDU, StaticHeader(variance_hdr), data),
-            HDUFactory(CompImageHDU, StaticHeader(mask_hdr), data)
+            HDUFactory(CompImageHDU, StaticHeader(mask_hdr), data),
         ]
 
         super().__init__(layout)
+
+
+class SimpleFits(HDUListFactory):
+    base_primary = {
+        "EXTNAME": "PRIMARY",
+        "NAXIS": 0,
+        "BITPIX": 8,
+        "OBS-MJD": 58914.0,
+        "NEXTEND": 3,
+        "OBS-LAT": -30.166,
+        "OBS-LONG": -70.814,
+        "STD": "SimpleFits",
+    }
+
+    base_ext = {"NAXIS": 2, "NAXIS1": 2048, "NAXIS2": 4096, "BITPIX": 32}
+
+    base_wcs = {
+        "crpix": [1024.0, 2048.0],
+        "crval": [351, -5],
+        "ctype": ["RA---TAN", "DEC--TAN"],
+        "cunit": ["deg", "deg"],
+        "radesys": "ICRS",
+        "cd": [[-1.44e-07, 7.32e-05], [7.32e-05, 1.44e-05]],
+    }
+
+    n_static_sources = 100
+    base_src_cat_params = {
+        "amplitude": [500, 2000],
+        "x_mean": [0, 4096],
+        "y_mean": [0, 2048],
+        "x_stddev": [1, 2],
+        "y_stddev": [1, 2],
+        "theta": [0, np.pi]
+    }
+
+    n_moving_objects = 10
+    base_obj_cat_params = {
+        "amplitude": [500, 1000],
+        "x_mean": [0, 4096],
+        "y_mean": [0, 2048],
+        "vx": [300, 1000],
+        "vy": [300, 1000],
+        "stddev": [1, 10],
+        "theta": [0, np.pi]
+    }
+
+    # multiple options on how to handle callbacks as class members
+    # callbacks=[lambda old: old+0.01, ]
+    # callbacks=[functools.partial(self.increment_obstime, dt=0.001), ]
+    @callback
+    @staticmethod
+    def increment_obstime(old, dt):
+        return old + dt
+
+    def __init__(self, shape=(2048, 4096), noise=100, dt=0.001,
+                 add_static_sources=False, src_cat="random", src_cat_params=None,
+                 add_moving_objects=False, obj_cat="random", obj_cat_params=None,
+                 **kwargs):
+        # let's just make sure the base headers align with the basic params
+        self.base_ext["NAXIS1"] = shape[0]
+        self.base_ext["NAXIS2"] = shape[1]
+        self.base_wcs["crpix"] = (int(shape[0]/2), int(shape[1]/2))
+        self.base_src_cat_params["y_mean"][1] = shape[0]
+        self.base_src_cat_params["x_mean"][1] = shape[1]
+        self.base_obj_cat_params["y_mean"][1] = shape[0]
+        self.base_obj_cat_params["x_mean"][1] = shape[1]
+        self.dt = dt
+
+        # internal counter of n images created so far
+        self.idx = 0
+
+        # First we gen the catalogs we need to generate data
+        if add_static_sources and src_cat == "random":
+            src_params = self.base_src_cat_params if src_cat_params is None else src_cat_params
+            n = src_params.pop("n", self.n_static_sources)
+            src_cat = gen_random_static_source_catalog(n, src_params)
+        if not add_static_sources:
+            src_cat = None
+
+        if add_moving_objects and obj_cat == "random":
+            obj_params = self.base_obj_cat_params if obj_cat_params is None else obj_cat_params
+            n = obj_params.pop("n", self.n_moving_objects)
+            obj_cat = gen_random_moving_object_catalog(n, obj_params, dt)
+        if not add_moving_objects:
+            obj_cat = None
+        self.obj_cat = obj_cat
+
+        # Then we can generate The header and data factories that build our
+        # HDUList.  Primary header contains no data, but does contain
+        # update-able metadata fields (f.e. timestamps)
+        primary_hdr = self.gen_primary_hdr()
+        primary_header_factory = MutableHeader(
+            metadata=primary_hdr,
+            mutables=[
+                "OBS-MJD",
+            ],
+            callbacks=[
+                self.increment_obstime(dt=dt),
+            ],
+        )
+
+        # The remaining HDUs contain static headers, but data that changes
+        # (f.e. updating positions of linearly moving objects in each image)
+        img_hdr = self.gen_ext_hdr({"EXTNAME": "IMAGE"}, add_wcs=True)
+        img_data = SimpleImage.from_simplistic_sim(
+            shape=shape,
+            noise=noise,
+            catalog=src_cat,
+            return_copy = True if add_moving_objects else False
+        )
+        var_hdr = self.gen_ext_hdr({"EXTNAME": "VARIANCE"}, add_wcs=True)
+        var_data = SimpleVariance(
+            img_data.base_image,
+            read_noise=noise,
+            gain=1.0
+        )
+        mask_hdr = self.gen_ext_hdr({"EXTNAME": "MASK", "BITPIX": 8}, add_wcs=True)
+        mask_data = ZeroedData(np.zeros(shape))
+
+        # Now we can build an HDU layout and let the HDUList factory
+        # generate self-updateable HDULists.
+        layout = [
+            HDUFactory(PrimaryHDU, primary_header_factory),
+            HDUFactory(CompImageHDU, StaticHeader(img_hdr), img_data),
+            HDUFactory(CompImageHDU, StaticHeader(var_hdr), var_data),
+            HDUFactory(CompImageHDU, StaticHeader(mask_hdr), mask_data),
+        ]
+
+        super().__init__(layout)
+
+    def mock(self, **kwargs):
+        hdul = super().mock(**kwargs)
+
+        if self.obj_cat is not None:
+            # advance the catalog:
+            self.obj_cat["x_mean"] += self.obj_cat["vx"]*self.dt
+            self.obj_cat["y_mean"] += self.obj_cat["vy"]*self.dt
+        new_scene = add_model_objects(
+            hdul["IMAGE"].data,
+            self.obj_cat,
+            models.Gaussian2D(x_stddev=1, y_stddev=1),
+            oversample=1
+        )
+        hdul["IMAGE"].data = new_scene
+        return hdul
 
 
 class DECamImdiffs(HDUListFactory):
--- src/kbmod/mocking/fits_data.py
+++ src/kbmod/mocking/fits_data.py
@@ -8,11 +8,15 @@ from astropy.io.fits import (
     BinTableHDU,
     TableHDU
 )
+from astropy.modeling import models
+from astropy.convolution import discretize_model
 
 
 __all__ = [
+    "add_model_objects",
     "DataFactory",
     "ZeroedData",
+    "SimpleImage",
     "SimpleMask"
 ]
 
@@ -38,8 +42,11 @@ class DataFactory(abc.ABC):
 
 
 class ZeroedData(DataFactory):
-    def __init__(self):
+    def __init__(self, base_image=None, return_copy=False):
         super().__init__()
+        self.base_image = base_image
+        self.base_image.flags.writeable = False
+        self.return_copy = return_copy
 
     def mock_image_data(self, hdu):
         cols = hdu.header.get("NAXIS1", False)
@@ -61,7 +68,14 @@ class ZeroedData(DataFactory):
         nrows = hdu.header["TFIELDS"]
         return np.zeros((nrows,), dtype=hdu.data.dtype)
 
+    def mock_static(self, hdu=None):
+        if self.return_copy:
+            return self.base_image.copy()
+        return self.base_image
+
     def mock(self, hdu=None, **kwargs):
+        if self.base_image is not None:
+            return self.mock_static(hdu)
         if isinstance(hdu, (PrimaryHDU, CompImageHDU, ImageHDU)):
             return self.mock_image_data(hdu)
         elif isinstance(hdu, (TableHDU, BinTableHDU)):
@@ -70,23 +84,100 @@ class ZeroedData(DataFactory):
             raise TypeError(f"Expected an HDU, got {type(hdu)} instead.")
 
 
+def add_model_objects(img, catalog, model, oversample=1):
+    shape = img.shape
+    yidx, xidx = np.indices(shape)
+
+    # find catalog columns that exist for the model
+    params_to_set = []
+    for param in catalog.colnames:
+        if param in model.param_names:
+            params_to_set.append(param)
+
+    # Save the initial model parameters so we can set them back
+    init_params = {param: getattr(model, param) for param in params_to_set}
+
+    try:
+        for i, source in enumerate(catalog):
+            for param in params_to_set:
+                setattr(model, param, source[param])
+            model.render(img)
+    finally:
+        for param, value in init_params.items():
+            setattr(model, param, value)
+
+    return img
+
+
+class SimpleImage(DataFactory):
+    rng = np.random.default_rng(12345)
+    read_noise_gen = rng.normal
+    dark_current_gen = rng.poisson
+    sky_count_gen = rng.poisson
+
+    def __init__(self, image, return_copy=False):
+        self.base_image = image
+        self.base_image.flags.writeable = False
+        self.return_copy = return_copy
+
+    def mock(self, hdu=None, **kwargs):
+        if self.return_copy:
+            return self.base_image.copy()
+        return self.base_image
+
+    @classmethod
+    def from_simplistic_sim(cls, shape, noise, catalog=None,
+                            src_model=models.Gaussian2D(x_stddev=1, y_stddev=1),
+                            oversample=1, **kwargs):
+        # create poisson background
+        img = cls.sky_count_gen(noise, size=shape).astype(np.float32)
+
+        # add catalog objects
+        if catalog is None:
+            return cls(img, **kwargs)
+
+        img = add_model_objects(img, catalog, src_model, oversample)
+        return cls(img, **kwargs)
+
+
+class SimpleVariance(DataFactory):
+    def __init__(self, image, read_noise, gain, return_copy=False):
+        self.base_image = image/gain + read_noise**2
+        self.return_copy = return_copy
+
+    def mock(self, hdu=None, **kwargs):
+        if self.return_copy:
+            return self.base_image.copy()
+        else:
+            return self.base_image
+
+
 class SimpleMask(DataFactory):
-    def __init__(self, mask):
+    def __init__(self, mask, return_copy=False):
         super().__init__()
 
         # not sure if this is "best" "best" way, but
         # it does safe a lot of array copies if we don't
-        # have to write to the array
+        # have to write to the mocked array (which we shouldn't?)
         self.mask = mask
         self.mask.writeable = False
+        self.return_copy = return_copy
 
     @classmethod
-    def from_params(cls, shape, padding=0):
+    def from_params(cls, shape, padding=0, bad_columns=[]):
         mask = np.zeros(shape)
+
+        # padding
         mask[:padding] = 1
         mask[shape[0]-padding:] = 1
         mask[:, :padding] = 1
         mask[: shape[1]-padding:] = 1
+
+
+        # bad columns
+        for col in bad_columns:
+            mask[:, col] = 1
+
         return cls(mask)
 
     # bro I don't know antymore
@@ -103,3 +194,94 @@ class SimpleMask(DataFactory):
                                  f"slice, got {patch} instead.")
 
         return cls(mask)
+
+    def mock(self, hdu=None, **kwargs):
+        if self.return_copy:
+            return self.mask.copy()
+        return self.mask
+
+
+class SimulatedImage(DataFactory):
+    rng = np.random.default_rng(12345)
+    read_noise_gen = rng.normal
+    dark_current_gen = rng.poisson
+    sky_count_gen = rng.poisson
+
+    def __add_bad_cols(self, image, cols, bias, n, seed, pattern_offset):
+        if not cols:
+            # most of the time I imagine we don't need bad cols
+            return image
+
+        if bad_columns == "random":
+            rng = np.random.RandomState(seed=self.bad_cols_seed)
+            bad_cols = rng.randint(0, shape[1], size=n)
+        else:
+            bad_cols = bad_columns
+
+        self.col_pattern = rng.randint(0, int(pattern_offset * bias), size=shape[0])
+        for col in columns:
+            image[:, col] = bias + col_pattern
+
+        return image
+
+    def __add_hot_pixels(self, image, pixels, percent, offset):
+        if not pixels:
+            # most of the time I imagine we don't need hot pixels
+            return image
+
+        if pixels == "random":
+            rng = np.random.RandomState(seed=self.hot_pixel_seed)
+            n_pixels = image.shape[0] * image.shape[1]
+            n = int(percent * n_pixels)
+            x = rng.randint(0, shape[1], size=n)
+            y = rng.randint(0, shape[0], size=n)
+            hot_pixels = np.column_stack(x, y)
+        else:
+            hot_pixels = pixels
+
+        for pix in hot_pixels:
+            image[*pix] += image[*pix]*offset
+
+        return image
+
+
+    def __init__(
+            self,
+            shape,
+            read_noise, gain,
+            bias,
+            exposure_time, dark_current,
+            sky_level,
+            bad_columns=False, bad_cols_seed=321, n_bad_cols=5, bad_col_pattern_offset=0.1,
+            hot_pixels=False, hot_pix_seed=543, hot_percent=0.00001, hot_offset=1000,
+    ):
+        super().__init__()
+
+        # starting empty image
+        self.base_img = np.zeros(shape)
+
+        # add read noise
+        self.base_img += self.read_noise_gen(scale=read_noise/gain, size=shape)
+
+        # add bias
+        self.base_img += bias
+
+        # add bad columns
+        self.bad_cols_seed = bad_cols_seed
+        self.__add_bad_cols(self.base_image, bad_columns, bias, n_bad_cols, bad_cols_seed)
+
+        # add dark current
+        current = dark_current * exposure_time/gain
+        self.base_img = self.dark_current_gen(current, size=shape)
+
+        # add hot pixels
+        self.hot_pixel_seed = hot_pix_seed
+        self.base_img = self.__add_hot_pixels(self.base_img, hot_pixels, hot_percent, hot_offset)
+
+        # add sky counts
+        self.base_img += self.sky_count_gen(sky_level*gain, shape)/gain
+
+    def mock(self, hdu=None, **kwargs):
+        # we do always have to return a new copy here, since sci images
+        # are expected to be written on
+        return self.base_img.copy()
