ad107c1706465ffa5eca4ebe690cb1a8cf70141e
DinoBektesevic <dinob@uw.edu>
2024-03-24 03:49:03 -0700

    Refactor, fix bugs with noise, tidy up.
    Join the Static and Mutable headers together. This halves the code
    length, but then the validation method triples it...
    Clean up and replace Static and Mutable with HeaderFactory.
    
    Move Header base skeleton generators to HeaderFactory from HDUList.
    Clean up their use in fits.py.
    
    Compute image size from NAXIS keywords when creating base Header
    skeletons.
    
    Move image rendering logic from HDUList to DataFactory. Clean up
    fits.py after the change.
    
    Fix EmptyFits generation.
    
    Fix SimpleImage noise issue.
    
    Clean up imports.
    
    Blacken.

--- src/kbmod/mocking/headers.py
+++ src/kbmod/mocking/headers.py
@@ -1,57 +1,111 @@
-import abc
-import types
 import warnings
-import functools
 
-import numpy as np
 from astropy.utils.exceptions import AstropyUserWarning
 from astropy.wcs import WCS
-from astropy.io.fits import HDUList, PrimaryHDU, CompImageHDU, ImageHDU, BinTableHDU, TableHDU, Header
+from astropy.io.fits import Header
 
 from .utils import header_archive_to_table
 
 
 __all__ = [
     "HeaderFactory",
-    "StaticHeader",
-    "MutableHeader",
     "ArchivedHeader",
 ]
 
 
-class HeaderFactory(abc.ABC):
-    @abc.abstractmethod
-    def mock(self, hdu=None, **kwargs):
-        raise NotImplementedError()
+class HeaderFactory:
+    base_primary = {
+        "EXTNAME": "PRIMARY",
+        "NAXIS": 0,
+        "BITPIX": 8,
+        "OBS-MJD": 58914.0,
+        "NEXTEND": 3,
+        "OBS-LAT": -30.166,
+        "OBS-LONG": -70.814,
+    }
 
+    base_ext = {"NAXIS": 2, "NAXIS1": 2048, "NAXIS2": 4096, "BITPIX": 32}
 
-class StaticHeader(HeaderFactory):
-    def __init__(self, metadata=None):
-        self.metadata = metadata
+    base_wcs = {
+        "ctype": ["RA---TAN", "DEC--TAN"],
+        "crval": [351, -5],
+        "cunit": ["deg", "deg"],
+        "radesys": "ICRS",
+        "cd": [[-1.44e-07, 7.32e-05], [7.32e-05, 1.44e-05]],
+    }
 
-    def mock(self, hdu=None, **kwargs):
-        # I have no idea why cards are an empty list by default
-        cards = [] if self.metadata is None else self.metadata
-        return Header(cards=cards)
+    def __validate_mutables(self):
+        # !xor
+        if bool(self.mutables) != bool(self.callbacks):
+            raise ValueError(
+                "When providing a list of mutable cards, you must provide associated callback methods."
+            )
 
+        if self.mutables is None:
+            return
 
-class MutableHeader(HeaderFactory):
-    def __init__(self, metadata=None, mutables=None, callbacks=None):
-        for k in mutables:
-            if k not in metadata:
+        if len(self.mutables) != len(self.callbacks):
+            raise ValueError(
+                "The number of mutable cards does not correspond to the number of given callbacks."
+            )
+
+        for k in self.mutables:
+            if k not in self.header:
                 raise ValueError(
-                    f"Registered mutable key {k} does not exists " "in given metadata: f{metadata}."
+                    f"Mutable key {k} does not exists "
+                    "in the header. Please "
+                    "provide the required metadata keys."
                 )
 
-        self.metadata = metadata
+    def __init__(self, metadata=None, mutables=None, callbacks=None):
+        cards = [] if metadata is None else metadata
+        self.header = Header(cards=cards)
+
         self.mutables = mutables
         self.callbacks = callbacks
+        self.__validate_mutables()
 
     def mock(self, hdu=None, **kwargs):
-        for i, mutable in enumerate(self.mutables):
-            self.metadata[mutable] = self.callbacks[i](self.metadata[mutable])
+        if self.mutables is not None:
+            for i, mutable in enumerate(self.mutables):
+                self.header[mutable] = self.callbacks[i](self.header[mutable])
 
-        return Header(self.metadata)
+        return self.header
+
+    @classmethod
+    def gen_wcs(cls, crval, metadata=None):
+        metadata = cls.base_wcs if metadata is None else metadata
+        wcs = WCS(naxis=2)
+        for k, v in metadata.items():
+            setattr(wcs.wcs, k, v)
+        return wcs.to_header()
+
+    @classmethod
+    def gen_header(cls, base, metadata, extend, add_wcs):
+        header = Header(base) if extend else Header()
+        header.update(metadata)
+
+        if add_wcs:
+            naxis1 = header.get("NAXIS1", False)
+            naxis2 = header.get("NAXIS2", False)
+            if not all((naxis1, naxis2)):
+                raise ValueError("Adding a WCS to the header requires NAXIS1 and NAXIS2 keys.")
+            crpix = [naxis1 / 2.0, naxis2 / 2.0]
+            header.update(cls.gen_wcs(crpix))
+
+        return header
+
+    @classmethod
+    def from_base_primary(cls, metadata=None, mutables=None, callbacks=None, extend_base=True, add_wcs=False):
+        hdr = cls.gen_header(cls.base_primary, metadata, extend_base, add_wcs)
+        return cls(hdr, mutables, callbacks)
+
+    @classmethod
+    def from_base_ext(
+        cls, metadata=None, mutables=None, callbacks=None, extend_base=True, add_wcs=True, dims=None
+    ):
+        hdr = cls.gen_header(cls.base_ext, metadata, extend_base, add_wcs)
+        return cls(hdr, mutables, callbacks)
 
 
 class ArchivedHeader(HeaderFactory):
