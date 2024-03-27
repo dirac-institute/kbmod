a20295312924092baabe946e2a24ad7c85ecdffd
Wilson Beebe <wbeebe@uw.edu>
2024-03-27 12:56:55 -0700

    Collect VDR data from butler

---
+++ src/kbmod/region_search.py
@@ -0,0 +1,313 @@
+try:
+    import lsst.daf.butler as dafButler
+except ImportError:
+    raise ImportError("LSST stack not found. Please install the LSST stack to use this module.")
+
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+import pandas as pd
+
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+from kbmod import ImageCollection
+
+
+def _chunked_data_ids(dataIds, chunk_size=200):
+    """Helper function to yield successive chunk_size chunks from dataIds."""
+    for i in range(0, len(dataIds), chunk_size):
+        yield dataIds[i : i + chunk_size]
+
+
+"""
+A class for searching through a dataset for data suitable for KBMOD processing,
+
+With a path to a butler repository, it provides helper methods for basic exploration of the data,
+methods for retrieving data from the butler for search, and transformation of the data
+into a KBMOD ImageCollection for further processing.
+"""
+
+
+class RegionSearch:
+    def __init__(
+        self,
+        repo_path,
+        collections,
+        dataset_types,
+        butler=None,
+        visit_info_str="Exposure.visitInfo",
+        parallel=False,
+        fetch_data=False,
+    ):
+        """
+        Parameters
+        ----------
+        repo_path : str
+            The path to the LSST butler repository.
+        collections : list of str
+            The list of desired collection names within the Butler repository`
+        dataset_types : list of str
+            The list of desired dataset types within the Butler repository.
+        butler : `lsst.daf.butler.Butler`, optional
+            The Butler object to use for data access. If None, a new Butler object will be created from `repo_path`.
+        visit_info_str : str
+            The name used when querying the butler for VisitInfo for exposures. Default is "visitInfo".
+        parallel : bool
+            If True, use parallel processing where possible. Note that each parallel worker
+            will instantiate its own Butler objects, Default is False.
+        fetch_data: bool, optional
+            If True, fetch the VDR data when the object is created. Default is True.
+        """
+        self.repo_path = repo_path
+        if butler is not None:
+            self.butler = butler
+        else:
+            self.butler = dafButler.Butler(self.repo_path)
+
+        self.collections = collections
+        self.dataset_types = dataset_types
+        self.visit_info_str = visit_info_str
+        self.parallel = parallel
+
+        if not fetch_data:
+            self.vdr_data = pd.DataFrame()
+        else:
+            self.vdr_data = self.fetch_vdr_data()
+            self.vdr_data["center_coord"] = [
+                self.get_center_ra_dec(region) for region in self.vdr_data["region"]
+            ]
+            self.vdr_data["uri"] = self.get_uris(self.vdr_data["data_id"])
+
+    @staticmethod
+    def get_collection_names(butler=None, repo_path=None):
+        """
+        Get the list of the names of available collections in a butler repository.
+        Parameters
+        ----------
+        butler | repo_path : `lsst.daf.butler.Butler` | str
+            The Butler object or a path to the LSST butler repository from which to create a butler.
+        Returns
+        -------
+        list of str
+            The list of available collections in the butler repository.
+        """
+        if butler is None:
+            if repo_path is None:
+                raise ValueError("Must specify one of repo_path or butler")
+            butler = dafButler.Butler(repo_path)
+        return butler.registry.queryCollections()
+
+    @staticmethod
+    def get_dataset_type_freq(butler=None, repo_path=None, collections=None):
+        """
+        Get the frequency of refs per dataset types across the given collections.
+
+        Parameters
+        ----------
+        butler | repo_path : `lsst.daf.butler.Butler` | str
+            The Butler object or a path to the LSST butler repository from which to create a butler.
+        collections : list of str, optional
+            The names of collections from which we can querry the dataset type frequencies. If None, use all collections.
+        Returns
+        -------
+        dict
+            A dictionary of frequency of refs per dataset type in the given collections.
+        """
+        if butler is None:
+            if repo_path is None:
+                raise ValueError("Must specify one of repo_path or butler")
+            butler = dafButler.Butler(repo_path)
+
+        # Iterate over all dataset types and count the frequency of refs associated with each
+        freq = {}
+        for dt in butler.registry.queryDatasetTypes():
+            refs = None
+            if collections:
+                refs = butler.registry.queryDatasets(dt, collections=collections)
+            else:
+                refs = butler.registry.queryDatasets(dt)
+            if refs is not None:
+                if dt.name not in freq:
+                    freq[dt.name] = 0
+                freq[dt.name] += refs.count(exact=True, discard=True)
+
+        return freq
+
+    def new_butler(self):
+        """Instantiates a new Butler object from the repo_path."""
+        return dafButler.Butler(self.repo_path)
+
+    def set_collections(self, collections):
+        """
+        Set which collections to use when querying data from the butler.
+
+        Parameters
+        ----------
+        collections : list of str
+            The list of desired collections to use for the region search.
+        """
+        self.collections = collections
+
+    def set_dataset_types(self, dataset_types):
+        """
+        Set the desired dataset types to use when querying the butler.
+        """
+        self.dataset_types = dataset_types
+
+    def get_vdr_data(self):
+        """Returns the VDR data"""
+        return self.vdr_data
+
+    def fetch_vdr_data(self, collections=None, dataset_types=None):
+        """
+        Constructs the VDR (Visit Detector Region) data for the given collections and dataset types.
+
+        VDRs are the regions of the detector that are covered by a visit. They contain what we need in terms of
+        regions hashes and unique dataIds.
+
+        Parameters
+        ----------
+        collections : list(str)
+            The names of the collection to get the dataset type stats for. If None, use self.collections.
+        dataset_types : list(str)
+            The names of the dataset types to get the dataset type stats for. If None, use self.dataset_types.
+        """
+        if not collections:
+            if not self.collections:
+                raise ValueError("No collections specified")
+            collections = self.collections
+
+        if not dataset_types:
+            if not self.dataset_types:
+                raise ValueError("No dataset types specified")
+            dataset_types = self.dataset_types
+
+        vdr_dict = {"data_id": [], "region": [], "detector": []}
+
+        for dt in dataset_types:
+            refs = self.butler.registry.queryDimensionRecords(
+                "visit_detector_region", datasets=dt, collections=collections
+            )
+            for ref in refs:
+                vdr_dict["data_id"].append(ref.dataId)
+                vdr_dict["region"].append(ref.region)
+                vdr_dict["detector"].append(ref.detector)
+
+        # return as a pandas dataframe
+        return pd.DataFrame(vdr_dict)
+
+    def get_instruments(self, data_ids, first_instrument_only=False):
+        """
+        Get the instruments for the given VDR dataIds.
+
+        Parameters
+        ----------
+        data_ids : list(dict)
+            The list of VDR data IDs to get the instruments for.
+        first_instrument_only : bool
+            If True, return only the first instrument we find.
+        """
+        instruments = []
+        for dataId in data_ids:
+            instrument = self.butler.get(self.visit_info_str, dataId=dataId, collections=self.collections)
+            if first_instrument_only:
+                return [instrument]
+            instruments.append(instrument)
+        return instruments
+
+    def get_uris_serial(self, data_ids, dataset_types=None, collections=None, butler=None):
+        """Fetch URIs for a list of dataIds in serial fashion.
+
+        Parameters
+        ----------
+        data_ids : list(dict)
+            The list of data Ids to fetch URIs for.
+        dataset_types : list(str)
+            The dataset types to use when fetching URIs. If None, use self.dataset_types.
+        collections : list(str)
+            The collections to use when fetching URIs. If None, use self.collections.
+        butler : `lsst.daf.butler.Butler`, optional
+            The Butler object to use for data access. If None, use self.butler.
+
+        Returns
+        -------
+        list(str)
+            The list of URIs for the given data Ids.
+        """
+        if butler is None:
+            butler = self.butler
+        if dataset_types is None:
+            if self.dataset_types is None:
+                raise ValueError("No dataset types specified")
+            dataset_types = self.dataset_types
+        if collections is None:
+            if self.collections is None:
+                raise ValueError("No collections specified")
+            collections = self.collections
+
+        results = []
+        for data_id in data_ids:
+            try:
+                uri = self.butler.getURI(dataset_types[0], dataId=data_id, collections=collections)
+                uri = uri.geturl()  # Convert to URL string
+                results.append(uri)
+            except Exception as e:
+                print(f"Failed to retrieve path for dataId {data_id}: {e}")
+        return results
+
+    def get_uris(self, data_ids, dataset_types=None, collections=None):
+        """
+        Get the URIs for the given dataIds.
+
+        Parameters
+        ----------
+        data_ids : list(dict)
+            The list of data Ids to fetch URIs for.
+        dataset_types : list(str)
+            The dataset types to use when fetching URIs. If None, use self.dataset_types.
+        collections : list(str)
+            The collections to use when fetching URIs. If None, use self.collections.
+
+        Returns
+        -------
+        list(str)
+            The list of URIs for the given data Ids.
+        """
+        if dataset_types is None:
+            if self.dataset_types is None:
+                raise ValueError("No dataset types specified")
+            dataset_types = self.dataset_types
+        if collections is None:
+            if self.collections is None:
+                raise ValueError("No collections specified")
+            collections = self.collections
+
+        if not self.parallel:
+            return self.get_uris_serial(data_ids, dataset_types, collections)
+
+        # Divide the data_ids into chunks to be processed in parallel
+        data_id_chunks = list(_chunked_data_ids(data_ids))
+
+        # Use a ProcessPoolExecutor to fetch URIs in parallel
+        result_uris = []
+        with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
+            futures = [
+                executor.submit(self.get_uris_serial, chunk, self.new_butler(), dataset_types, collections)
+                for chunk in data_id_chunks
+            ]
+            for future in as_completed(futures):
+                result_uris.extend(future.result())
+
+        return result_uris
+
+    def get_center_ra_dec(self, region):
+        """
+        Get the center RA and Dec for the given region.
+        """
+        # Note we get the 2D boundingBox (not the boundingBox3d) from a region.
+        # We then extract the RA and Dec from the center of the bounding box.
+        bbox_center = region.getBoundingBox().getCenter()
+        ra = bbox_center.getLon().asDegrees()
+        dec = bbox_center.getLat().asDegrees()
+        return ra, dec
---
+++ tests/test_region_search.py
@@ -0,0 +1,177 @@
+import unittest
+
+import numpy as np
+from utils.utils_for_tests import get_absolute_data_path
+
+# A path for our mock repository
+MOCK_REPO_PATH = "far/far/away"
+
+from unittest import mock
+from utils import DatasetRef, DatasetId, dafButler, MockButler
+
+from kbmod import region_search
+
+
+@mock.patch.dict(
+    "sys.modules",
+    {
+        "lsst.daf.butler": dafButler,
+        "lsst.daf.butler.core.DatasetRef": DatasetRef,
+        "lsst.daf.butler.core.DatasetId": DatasetId,
+    },
+)
+class TestRegionSearch(unittest.TestCase):
+    """
+    Test the region search functionality.
+    """
+
+    def setUp(self):
+        self.butler = MockButler(MOCK_REPO_PATH)
+        self.default_collections = self.butler.registry.queryCollections()[:2]
+        self.default_datasetTypes = [dt.name for dt in self.butler.registry.queryDatasetTypes()][:2]
+
+        self.rs = region_search.RegionSearch(
+            MOCK_REPO_PATH, self.default_collections, self.default_datasetTypes, butler=self.butler
+        )  # parallel is False by default
+
+        self.parallel_rs = region_search.RegionSearch(
+            MOCK_REPO_PATH,
+            self.default_collections,
+            self.default_datasetTypes,
+            butler=self.butler,
+            # parallel=True # TODO re-enable when there's a better way to instantiate a mock butler
+        )
+
+    def test_init(self):
+        """
+        Test that the region search object can be initialized.
+        """
+        # TODO add tests for the butler and parallel flags
+        rs = region_search.RegionSearch(MOCK_REPO_PATH, [], [], butler=self.butler, parallel=False)
+        self.assertTrue(rs is not None)
+
+    def test_init_with_fetch(self):
+        """
+        Test that the region search object can fetch data on initializaiton
+        """
+        rs = region_search.RegionSearch(
+            MOCK_REPO_PATH,
+            self.default_collections,
+            self.default_datasetTypes,
+            butler=self.butler,
+            fetch_data=True,
+        )
+
+        self.assertTrue(rs is not None)
+        data = rs.fetch_vdr_data()
+        self.assertGreater(len(data), 0)
+
+    def test_chunked_data_ids(self):
+        """
+        Test that the data_ids are chunked correctly.
+        """
+        # Generate a list of random data_ids
+        data_ids = [str(i) for i in range(100)]
+        chunk_size = 10
+        # Get all chunks from the generator
+        chunks = [id for id in region_search._chunked_data_ids(data_ids, chunk_size)]
+
+        for i in range(len(chunks)):
+            chunk = chunks[i]
+            self.assertEqual(len(chunk), chunk_size)
+            for j in range(len(chunk)):
+                self.assertEqual(chunk[j], data_ids[i * chunk_size + j])
+
+    def test_get_collection_names(self):
+        """
+        Test that the collection names are retrieved correctly.
+        """
+        with self.assertRaises(ValueError):
+            region_search.RegionSearch.get_collection_names(butler=None, repo_path=None)
+
+        self.assertGreater(
+            len(
+                region_search.RegionSearch.get_collection_names(butler=self.butler, repo_path=MOCK_REPO_PATH)
+            ),
+            0,
+        )
+
+    def test_set_collections(self):
+        """
+        Test that the desired collections are set correctly.
+        """
+        collection_names = region_search.RegionSearch.get_collection_names(
+            butler=self.butler, repo_path=MOCK_REPO_PATH
+        )
+        self.rs.set_collections(collection_names)
+        self.assertEqual(self.rs.collections, collection_names)
+
+    def test_get_dataset_type_freq(self):
+        """
+        Test that the dataset type frequency is retrieved correctly.
+        """
+        freq = self.rs.get_dataset_type_freq(butler=self.butler, collections=self.default_collections)
+        self.assertTrue(len(freq) > 0)
+        for dataset_type in freq:
+            self.assertTrue(freq[dataset_type] > 0)
+
+    def test_set_dataset_types(self):
+        """
+        Test that the desired dataset types are correctly set.
+        """
+        freq = self.rs.get_dataset_type_freq(butler=self.butler, collections=self.default_collections)
+
+        self.assertGreater(len(freq), 0)
+        dataset_types = list(freq.keys())[0]
+        self.rs.set_dataset_types(dataset_types=dataset_types)
+
+        self.assertEqual(self.rs.dataset_types, dataset_types)
+
+    def test_fetch_vdr_data(self):
+        """
+        Test that the VDR data is retrieved correctly.
+        """
+        # Get the VDR data
+        vdr_data = self.rs.fetch_vdr_data()
+        self.assertTrue(len(vdr_data) > 0)
+
+    def test_get_instruments(self):
+        """
+        Test that the instruments are retrieved correctly.
+        """
+        data_ids = self.rs.fetch_vdr_data()["data_id"].values
+        # Get the instruments
+        first_instrument = self.rs.get_instruments(data_ids, first_instrument_only=True)
+        self.assertEqual(len(first_instrument), 1)
+
+        # Now test the default where getting the first instrument is False.
+        instruments = self.rs.get_instruments(data_ids)
+        self.assertGreater(len(instruments), 1)
+
+    def test_get_uris_serial(self):
+        """
+        Test that the URIs are retrieved correctly in serial mode.
+        """
+        data_ids = self.rs.fetch_vdr_data()["data_id"]
+        # Get the URIs
+        uris = self.rs.get_uris(data_ids)
+        self.assertTrue(len(uris) > 0)
+
+    def test_get_uris_parallel(self):
+        """
+        Test that the URIs are retrieved correctly in parallel mode.
+        """
+        data_ids = self.parallel_rs.fetch_vdr_data()["data_id"]
+        # Get the URIs
+        uris = self.parallel_rs.get_uris(data_ids)
+        self.assertTrue(len(uris) > 0)
+
+    def test_get_center_ra_dec(self):
+        """
+        Test that the center RA and Dec are retrieved correctly.
+        """
+        region = self.rs.fetch_vdr_data()["region"][0]
+
+        # Get the center RA and Dec
+        center_ra_dec = self.rs.get_center_ra_dec(region)
+        self.assertTrue(len(center_ra_dec) > 0)
