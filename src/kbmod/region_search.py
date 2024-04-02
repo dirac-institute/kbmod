224d0ed4f5fd6d7d6c1c003d2f45a93195ebb841
Wilson Beebe <wbeebe@uw.edu>
2024-04-02 14:48:03 -0700

    Update doc strings

--- src/kbmod/region_search.py
+++ src/kbmod/region_search.py
@@ -14,21 +14,19 @@ def _chunked_data_ids(dataIds, chunk_size=200):
     for i in range(0, len(dataIds), chunk_size):
         yield dataIds[i : i + chunk_size]
 
-
-"""
-A class for searching through a dataset for data suitable for KBMOD processing,
-
-With a path to a butler repository, it provides helper methods for basic exploration of the data,
-methods for retrieving data from the butler for search, and transformation of the data
-into a KBMOD ImageCollection for further processing.
-
-Note that currently we store results from the butler in an Astropy Table. In the future,
-we will likely want to use a database for faster performance and to handle processing of
-datasets that are too large to fit in memory.
-"""
-
-
 class RegionSearch:
+    """
+    A class for searching through a dataset for data suitable for KBMOD processing,
+
+    With a path to a butler repository, it provides helper methods for basic exploration of the data,
+    methods for retrieving data from the butler for search, and transformation of the data
+    into a KBMOD ImageCollection for further processing.
+
+    Note that currently we store results from the butler in an Astropy Table. In the future,
+    we will likely want to use a database for faster performance and to handle processing of
+    datasets that are too large to fit in memory.
+    """
+
     def __init__(
         self,
         repo_path,
@@ -42,20 +40,20 @@ class RegionSearch:
         """
         Parameters
         ----------
-        repo_path : str
+        repo_path : `str`
             The path to the LSST butler repository.
-        collections : list of str
+        collections : `list(str)`
             The list of desired collection names within the Butler repository`
-        dataset_types : list of str
+        dataset_types : `list(str)`
             The list of desired dataset types within the Butler repository.
         butler : `lsst.daf.butler.Butler`, optional
             The Butler object to use for data access. If None, a new Butler object will be created from `repo_path`.
-        visit_info_str : str
+        visit_info_str : `str`
             The name used when querying the butler for VisitInfo for exposures. Default is "Exposure.visitInfo".
-        parallel : bool
+        parallel : `bool`
             If True, use parallel processing where possible. Note that each parallel worker
             will instantiate its own Butler objects, Default is False.
-        fetch_data: bool, optional
+        fetch_data: `bool`, optional
             If True, fetch the VDR data when the object is created. Default is True.
         """
         self.repo_path = repo_path
@@ -81,11 +79,11 @@ class RegionSearch:
         Get the list of the names of available collections in a butler repository.
         Parameters
         ----------
-        butler | repo_path : `lsst.daf.butler.Butler` | str
+        butler | repo_path : `lsst.daf.butler.Butler` | `str`
             The Butler object or a path to the LSST butler repository from which to create a butler.
         Returns
         -------
-        list of str
+        `list(str)`
             The list of available collections in the butler repository.
         """
         if butler is None:
@@ -103,11 +101,11 @@ class RegionSearch:
         ----------
         butler | repo_path : `lsst.daf.butler.Butler` | str
             The Butler object or a path to the LSST butler repository from which to create a butler.
-        collections : list of str, optional
+        collections : `list(str)`, optional
             The names of collections from which we can querry the dataset type frequencies. If None, use all collections.
         Returns
         -------
-        dict
+        ref_freq : `dict`
             A dictionary of frequency of refs per dataset type in the given collections.
         """
         if butler is None:
@@ -116,7 +114,7 @@ class RegionSearch:
             butler = dafButler.Butler(repo_path)
 
         # Iterate over all dataset types and count the frequency of refs associated with each
-        freq = {}
+        ref_freq = {}
         for dt in butler.registry.queryDatasetTypes():
             refs = None
             if collections:
@@ -124,11 +122,11 @@ class RegionSearch:
             else:
                 refs = butler.registry.queryDatasets(dt)
             if refs is not None:
-                if dt.name not in freq:
-                    freq[dt.name] = 0
-                freq[dt.name] += refs.count(exact=True, discard=True)
+                if dt.name not in ref_freq:
+                    ref_freq[dt.name] = 0
+                ref_freq[dt.name] += refs.count(exact=True, discard=True)
 
-        return freq
+        return ref_freq
 
     def new_butler(self):
         """Instantiates a new Butler object from the repo_path."""
@@ -140,7 +138,7 @@ class RegionSearch:
 
         Parameters
         ----------
-        collections : list of str
+        collections : `list(str)`
             The list of desired collections to use for the region search.
         """
         self.collections = collections
@@ -164,10 +162,15 @@ class RegionSearch:
 
         Parameters
         ----------
-        collections : list(str)
+        collections : `list(str)`
             The names of the collection to get the dataset type stats for. If None, use self.collections.
-        dataset_types : list(str)
+        dataset_types : `list(str)`
             The names of the dataset types to get the dataset type stats for. If None, use self.dataset_types.
+
+        Returns
+        -------
+        vdr_data : `astropy.table.Table`
+            An Astropy Table containing the VDR data and associated URIs and RA/Dec center coordinates.
         """
         if not collections:
             if not self.collections:
@@ -203,10 +206,15 @@ class RegionSearch:
 
         Parameters
         ----------
-        data_ids : list(dict), optional
-            The list of VDR data IDs to get the instruments for. By default uses previously fetched data_ids
-        first_instrument_only : bool
-            If True, return only the first instrument we find.
+        data_ids : `iterable(dict)`, optional
+            A collection of VDR data IDs to get the instruments for. By default uses previously fetched data_ids
+        first_instrument_only : `bool`, optional
+            If True, return only the first instrument we find. Default is False.
+
+        Returns
+        -------
+        instruments : `list(lsst.afw.instrument.Instrument)`
+            A list of instrument objects for the given data IDs.
         """
         if data_ids is None:
             data_ids = self.vdr_data["data_id"]
@@ -224,18 +232,18 @@ class RegionSearch:
 
         Parameters
         ----------
-        data_ids : list(dict)
-            The list of data Ids to fetch URIs for.
-        dataset_types : list(str)
+        data_ids : `iterable(dict)`
+            A collection of data Ids to fetch URIs for.
+        dataset_types : `list(str)`
             The dataset types to use when fetching URIs. If None, use self.dataset_types.
-        collections : list(str)
+        collections : `list(str)`
             The collections to use when fetching URIs. If None, use self.collections.
         butler : `lsst.daf.butler.Butler`, optional
             The Butler object to use for data access. If None, use self.butler.
 
         Returns
         -------
-        list(str)
+        uris : `list(str)`
             The list of URIs for the given data Ids.
         """
         if butler is None:
@@ -249,15 +257,15 @@ class RegionSearch:
                 raise ValueError("No collections specified")
             collections = self.collections
 
-        results = []
+        uris = []
         for data_id in data_ids:
             try:
                 uri = self.butler.getURI(dataset_types[0], dataId=data_id, collections=collections)
                 uri = uri.geturl()  # Convert to URL string
-                results.append(uri)
+                uris.append(uri)
             except Exception as e:
                 print(f"Failed to retrieve path for dataId {data_id}: {e}")
-        return results
+        return uris
 
     def get_uris(self, data_ids, dataset_types=None, collections=None):
         """
@@ -265,16 +273,16 @@ class RegionSearch:
 
         Parameters
         ----------
-        data_ids : list(dict)
-            The list of data Ids to fetch URIs for.
-        dataset_types : list(str)
+        data_ids : `iterable(dict)`
+            A collection of data Ids to fetch URIs for.
+        dataset_types : `list(str)`
             The dataset types to use when fetching URIs. If None, use self.dataset_types.
-        collections : list(str)
+        collections : `list(str)`
             The collections to use when fetching URIs. If None, use self.collections.
 
         Returns
         -------
-        list(str)
+        uris : `list(str)`
             The list of URIs for the given data Ids.
         """
         if dataset_types is None:
@@ -293,7 +301,7 @@ class RegionSearch:
         data_id_chunks = list(_chunked_data_ids(data_ids))
 
         # Use a ProcessPoolExecutor to fetch URIs in parallel
-        result_uris = []
+        uris = []
         with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
             futures = [
                 executor.submit(
@@ -306,13 +314,23 @@ class RegionSearch:
                 for chunk in data_id_chunks
             ]
             for future in as_completed(futures):
-                result_uris.extend(future.result())
+                uris.extend(future.result())
 
-        return result_uris
+        return uris
 
     def get_center_ra_dec(self, region):
         """
         Get the center RA and Dec for the given region.
+
+        Parameters
+        ----------
+        region : `lsst::sphgeom::Region Class Reference`
+            The region for which to get the center RA and Dec.
+
+        Returns
+        -------
+        ra, dec : `float`, `float`
+            The center RA and Dec in degrees.
         """
         # Note we get the 2D boundingBox (not the boundingBox3d) from a region.
         # We then extract the RA and Dec from the center of the bounding box.
