ba9f244f2d0b2238b0901c7c7e2d063ead2d78e0
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2024-04-03 13:54:33 -0400

    comprehensive testing of interpolated add

--- tests/test_raw_image.py
+++ tests/test_raw_image.py
@@ -124,10 +124,32 @@ class test_RawImage(unittest.TestCase):
         # Get the original value using (r, c) lookup.
         org_val17 = img.get_pixel(1, 7)
 
+        # Get the sum of this pixel and its immediate neighbors.
+        org_sum = np.sum(img.image[0:3, 6:9])
+
         # Interpolated add uses the cartesian coordinates (x, y)
         img.interpolated_add(7, 1, 10.0)
         self.assertLess(img.get_pixel(1, 7), org_val17 + 10.0)
         self.assertGreater(img.get_pixel(1, 7), org_val17 + 2.0)
+
+        # Test we have added a total of 10.0 to the pixel and its neighbors.
+        new_sum = np.sum(img.image[0:3, 6:9])
+        self.assertAlmostEqual(new_sum - org_sum, 10.0)
+
+        # Add values right near the edge of the pixel.
+        org_val21 = img.get_pixel(2, 1)
+        org_val31 = img.get_pixel(3, 1)
+        org_val32 = img.get_pixel(3, 2)
+        org_val41 = img.get_pixel(4, 1)
+        img.interpolated_add(1.9, 3.5, 10.0)
+
+        # All the value should go to (3, 1) and (3, 2) with more going to (3, 1)
+        diff31 = img.get_pixel(3, 1) - org_val31
+        diff32 = img.get_pixel(3, 2) - org_val32
+        self.assertAlmostEqual(diff31 + diff32, 10.0)
+        self.assertGreater(diff31, diff32)
+        self.assertAlmostEqual(org_val21, img.get_pixel(2, 1))
+        self.assertAlmostEqual(org_val41, img.get_pixel(4, 1))
 
     def test_approx_equal(self):
         """Test RawImage pixel value setters."""
