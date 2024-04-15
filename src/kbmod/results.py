2a4677995cf0d8f0a03c3197d917334184ec80f2
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2024-04-15 09:57:11 -0400

    Address some PR comments

---
+++ src/kbmod/results.py
@@ -0,0 +1,451 @@
+"""Results is a column-based data structure for tracking results with additional global data
+and helper functions for filtering and maintaining consistency between different attributes in each row.
+"""
+
+import numpy as np
+from pathlib import Path
+
+from astropy.table import Table, vstack
+
+from kbmod.trajectory_utils import make_trajectory, update_trajectory_from_psi_phi
+from kbmod.search import Trajectory
+
+
+class Results:
+    """This class stores a collection of related data from all of the kbmod results.
+
+    At a minimum it contains columns for the trajectory information:
+    (x, y, vx, vy, likelihood, flux, obs_count)
+    but additional columns can be added as needed.
+
+    Attributes
+    ----------
+    table : `astropy.table.Table`
+        The stored results data.
+    track_filtered : `bool`
+        Whether to track (save) the filtered trajectories. This will use
+        more memory and is recommended only for analysis.
+    filtered : `dict`
+        A dictionary mapping a string of filtered name to a table
+        of the results removed by that filter.
+    """
+
+    _required_cols = ["x", "y", "vx", "vy", "likelihood", "flux", "obs_count"]
+
+    def __init__(self, trajectories, track_filtered=False):
+        """Create a ResultTable class.
+
+        Parameters
+        ----------
+        trajectories : `list[Trajectory]`
+            A list of trajectories to include in these results.
+        track_filtered : `bool`
+            Whether to track (save) the filtered trajectories. This will use
+            more memory and is recommended only for analysis.
+        """
+        # Set up information to track which row is filtered at which round.
+        self.track_filtered = track_filtered
+        self.filtered = {}
+
+        # Create dictionaries for the required columns.
+        input_d = {}
+        invalid_d = {}
+        for col in self._required_cols:
+            input_d[col] = []
+            invalid_d[col] = []
+
+        # Add the valid trajectories to the table. If we are tracking filtered
+        # data, add invalid trajectories to the invalid_d dictionary.
+        for trj in trajectories:
+            if trj.valid:
+                input_d["x"].append(trj.x)
+                input_d["y"].append(trj.y)
+                input_d["vx"].append(trj.vx)
+                input_d["vy"].append(trj.vy)
+                input_d["likelihood"].append(trj.lh)
+                input_d["flux"].append(trj.flux)
+                input_d["obs_count"].append(trj.obs_count)
+            elif track_filtered:
+                invalid_d["x"].append(trj.x)
+                invalid_d["y"].append(trj.y)
+                invalid_d["vx"].append(trj.vx)
+                invalid_d["vy"].append(trj.vy)
+                invalid_d["likelihood"].append(trj.lh)
+                invalid_d["flux"].append(trj.flux)
+                invalid_d["obs_count"].append(trj.obs_count)
+
+        self.table = Table(input_d)
+        if track_filtered:
+            self.filtered["invalid_trajectory"] = Table(invalid_d)
+
+    def __len__(self):
+        return len(self.table)
+
+    def __str__(self):
+        return str(self.table)
+
+    def __repr__(self):
+        return repr(self.table).replace("Table", "Results")
+
+    def _repr_html_(self):
+        return self.table._repr_html_().replace("Table", "Results")
+
+    def __getitem__(self, key):
+        return self.table[key]
+
+    @property
+    def colnames(self):
+        return self.table.colnames
+
+    @classmethod
+    def from_table(cls, data, track_filtered=False):
+        """Extract data from an astropy Table with the minimum trajectory information.
+
+        Parameters
+        ----------
+        data : `astropy.table.Table`
+            The input data.
+        track_filtered : `bool`
+            Indicates whether to track future filtered points.
+
+        Raises
+        ------
+        Raises a KeyError if any required columns are missing.
+        """
+        # Check that the minimum information is present.
+        for col in cls._required_cols:
+            if col not in data.colnames:
+                raise KeyError(f"Column {col} missing from input data.")
+
+        # Create an empty Results object and append the data table.
+        results = Results([], track_filtered=track_filtered)
+        results.table = data
+
+        return results
+
+    @classmethod
+    def from_dict(cls, input_dict, track_filtered=False):
+        """Extract data from a dictionary with the minimum trajectory information.
+
+        Parameters
+        ----------
+        input_dict : `dict`
+            The input data.
+        track_filtered : `bool`
+            Indicates whether to track future filtered points.
+
+        Raises
+        ------
+        Raises a KeyError if any required columns are missing.
+        """
+        return cls.from_table(Table(input_dict))
+
+    @classmethod
+    def read_table(cls, filename, track_filtered=False):
+        """Read the ResultList from a table file.
+
+        Parameters
+        ----------
+        filename : `str`
+            The name of the file to load.
+        track_filtered : `bool`
+            Indicates whether the object should track future filtered points.
+
+        Raises
+        ------
+        Raises a FileNotFoundError if the file is not found.
+        Raises a KeyError if any of the columns are missing.
+        """
+        if not Path(filename).is_file():
+            raise FileNotFoundError(f"File {filename} not found.")
+        data = Table.read(filename)
+        return Results.from_table(data, track_filtered=track_filtered)
+
+    def extend(self, results2):
+        """Append the results in a second `Results` object to the current one.
+
+        Parameters
+        ----------
+        results2 : `Results`
+            The data structure containing additional results to add.
+
+        Returns
+        -------
+        self : `Results`
+            Returns a reference to itself to allow chaining.
+
+        Raises
+        ------
+        Raises a ValueError if the columns of the results do not match.
+        """
+        if set(self.colnames) != set(results2.colnames):
+            raise ValueError("Column mismatch when merging results")
+
+        self.table = vstack([self.table, results2.table])
+
+        # When merging the filtered results extend lists with the
+        # same key and create new lists for new keys.
+        for key in results2.filtered.keys():
+            if key in self.filtered:
+                self.filtered[key] = vstack([self.filtered[key], results2.filtered[key]])
+            else:
+                self.filtered[key] = results2.filtered[key]
+
+        return self
+
+    def make_trajectory_list(self):
+        """Create a list of ``Trajectory`` objects.
+
+        Returns
+        -------
+        trajectories : `list[Trajectory]`
+            The ``Trajectory`` objects.
+        """
+        trajectories = [
+            make_trajectory(
+                x=row["x"],
+                y=row["y"],
+                vx=row["vx"],
+                vy=row["vy"],
+                flux=row["flux"],
+                lh=row["likelihood"],
+                obs_count=row["obs_count"],
+            )
+            for row in self.table
+        ]
+        return trajectories
+
+    def _update_likelihood(self):
+        """Update the likelihood related trajectory information from the
+        psi and phi information. Requires the existence of the columns
+        'psi_curve' and 'phi_curve' which can be set with add_psi_phi_data().
+        Uses the (optional) 'valid_indices' if it exists.
+
+        Raises
+        ------
+        Raises an IndexError if the necessary columns are missing.
+        """
+        if "psi_curve" not in self.table.colnames:
+            raise IndexError("Missing column 'phi_curve'. Use add_psi_phi_data()")
+        if "phi_curve" not in self.table.colnames:
+            raise IndexError("Missing column 'phi_curve'. Use add_psi_phi_data()")
+
+        num_rows = len(self.table)
+        num_times = len(self.table["phi_curve"][0])
+        if "index_valid" in self.table.colnames:
+            phi_sum = (self.table["phi_curve"] * self.table["index_valid"]).sum(axis=1)
+            psi_sum = (self.table["psi_curve"] * self.table["index_valid"]).sum(axis=1)
+            num_obs = self.table["index_valid"].sum(axis=1)
+        else:
+            phi_sum = self.table["phi_curve"].sum(axis=1)
+            psi_sum = self.table["psi_curve"].sum(axis=1)
+            num_obs = np.full((num_rows, 1), num_times)
+
+        non_zero = phi_sum != 0
+        self.table["likelihood"] = np.zeros((num_rows))
+        self.table["likelihood"][non_zero] = psi_sum[non_zero] / np.sqrt(phi_sum[non_zero])
+        self.table["flux"] = np.zeros((num_rows))
+        self.table["flux"][non_zero] = psi_sum[non_zero] / phi_sum[non_zero]
+        self.table["obs_count"] = num_obs
+
+    def add_psi_phi_data(self, psi_array, phi_array, index_valid=None):
+        """Append columns for the psi and phi data and use this to update the
+        relevant trajectory information.
+
+        Parameters
+        ----------
+        psi_array : `numpy.ndarray`
+            An array of psi_curves with one for each row.
+        phi_array : `numpy.ndarray`
+            An array of psi_curves with one for each row.
+        index_valid : `numpy.ndarray`, optional
+            An optional array of index_valid arrays with one for each row.
+
+        Returns
+        -------
+        self : `Results`
+            Returns a reference to itself to allow chaining.
+
+        Raises
+        ------
+        Raises a ValueError if the input arrays are not the same size as the table
+        or a given pair of rows in the arrays are not the same length.
+        """
+        if len(psi_array) != len(self.table):
+            raise ValueError("Wrong number of psi curves provided.")
+        if len(phi_array) != len(self.table):
+            raise ValueError("Wrong number of phi curves provided.")
+        self.table["psi_curve"] = psi_array
+        self.table["phi_curve"] = phi_array
+
+        if index_valid is not None:
+            # Make the data to match.
+            if len(index_valid) != len(self.table):
+                raise ValueError("Wrong number of index_valid lists provided.")
+            self.table["index_valid"] = index_valid
+
+        # Update the track likelihoods given this new information.
+        self._update_likelihood()
+
+        return self
+
+    def filter_mask(self, mask, label=None):
+        """Filter the rows in the ResultTable to only include those indices
+        that are marked True in the mask.
+
+        Parameters
+        ----------
+        mask : `list` or `numpy.ndarray`
+            A list the same length as the table with True/False indicating
+            which row to keep.
+        label : `str`
+            The label of the filtering stage to use. Only used if
+            we keep filtered trajectories.
+
+        Returns
+        -------
+        self : `Results`
+            Returns a reference to itself to allow chaining.
+        """
+        if self.track_filtered:
+            if label is None:
+                label = ""
+
+            if label in self.filtered:
+                self.filtered[label] = vstack([self.filtered[label], self.table[~mask]])
+            else:
+                self.filtered[label] = self.table[~mask]
+
+        # Do the actual filtering.
+        self.table = self.table[mask]
+
+        # Return a reference to the current object to allow chaining.
+        return self
+
+    def filter_by_index(self, rows_to_keep, label=None):
+        """Filter the rows in the ResultTable to only include those indices
+        in the list indices_to_keep.
+
+        Parameters
+        ----------
+        rows_to_keep : `list[int]`
+            The indices of the rows to keep.
+        label : `str`
+            The label of the filtering stage to use. Only used if
+            we keep filtered trajectories.
+
+        Returns
+        -------
+        self : `Results`
+            Returns a reference to itself to allow chaining.
+        """
+        row_set = set(rows_to_keep)
+        mask = np.array([i in row_set for i in range(len(self.table))])
+        self.filter_mask(mask, label)
+        return self
+
+    def get_filtered(self, label=None):
+        """Get the results filtered at a given stage or all stages.
+
+        Parameters
+        ----------
+        label : `str`
+            The filtering stage to use. If no label is provided,
+            return all filtered rows.
+
+        Returns
+        -------
+        results : `astropy.table.Table` or ``None``
+            A table with the filtered rows or ``None`` if there are no entries.
+        """
+        if not self.track_filtered:
+            raise ValueError("ResultTable filter tracking not enabled.")
+
+        result = None
+        if label is not None:
+            # Check if anything was filtered at this stage.
+            if label in self.filtered:
+                result = self.filtered[label]
+        else:
+            result = vstack([x for x in self.filtered.values()])
+
+        return result
+
+    def revert_filter(self, label=None, add_column=None):
+        """Revert the filtering by re-adding filtered ResultRows.
+
+        Note
+        ----
+        Filtered rows are appended to the end of the list. Does not return
+        the results to the original ordering.
+
+        Parameters
+        ----------
+        label : `str`
+            The filtering stage to use. If no label is provided,
+            revert all filtered rows.
+        add_column : `str`
+            If not ``None``, add a tracking column with the given name
+            that includes the original filtering reason.
+
+        Returns
+        -------
+        self : `Results`
+            Returns a reference to itself to allow chaining.
+
+        Raises
+        ------
+        ValueError if filtering is not enabled.
+        KeyError if label is unknown.
+        """
+        if not self.track_filtered:
+            raise ValueError("ResultTable filter tracking not enabled.")
+
+        # Make a list of labels to revert
+        if label is not None:
+            if label not in self.filtered:
+                raise KeyError(f"Unknown filtered label {label}")
+            to_revert = [label]
+        else:
+            to_revert = list(self.filtered.keys())
+
+        # If we don't have the tracking column yet, add it.
+        if add_column is not None and add_column not in self.table.colnames:
+            self.table[add_column] = [""] * len(self.table)
+
+        # Make a list of tables to merge.
+        table_list = [self.table]
+        for key in to_revert:
+            filtered_table = self.filtered[key]
+            if add_column is not None and len(filtered_table) > 0:
+                filtered_table[add_column] = [key] * len(filtered_table)
+            table_list.append(filtered_table)
+            del self.filtered[key]
+        self.table = vstack(table_list)
+
+        return self
+
+    def write_table(self, filename, overwrite=True, cols_to_drop=[]):
+        """Write the unfiltered results to a single (ecsv) file.
+
+        Parameter
+        ---------
+        filename : `str`
+            The name of the result file.
+        overwrite : `bool`
+            Overwrite the file if it already exists. [default: True]
+        cols_to_drop : `list`
+            A list of columns to drop (to save space). [default: []]
+        """
+        if len(cols_to_drop) > 0:
+            # Make a copy so we can modify the table
+            write_table = self.table.copy()
+
+            for col in cols_to_drop:
+                if col in write_table.colnames:
+                    write_table.remove_column(col)
+
+            # Write out the table.
+            write_table.write(filename, overwrite=overwrite)
+        else:
+            self.table.write(filename, overwrite=overwrite)
---
+++ tests/test_results.py
@@ -0,0 +1,263 @@
+import numpy as np
+import os
+import tempfile
+import unittest
+
+from astropy.table import Table
+from pathlib import Path
+
+from kbmod.results import Results
+from kbmod.search import Trajectory
+from kbmod.trajectory_utils import make_trajectory
+
+
+class test_results(unittest.TestCase):
+    def setUp(self):
+        self.num_entries = 10
+        self.input_dict = {
+            "x": [],
+            "y": [],
+            "vx": [],
+            "vy": [],
+            "flux": [],
+            "likelihood": [],
+            "obs_count": [],
+        }
+        self.trj_list = []
+
+        for i in range(self.num_entries):
+            trj = make_trajectory(
+                x=i, y=i + 0, vx=i - 2.0, vy=i + 5.0, flux=5.0 * i, lh=100.0 + i, obs_count=i
+            )
+            self.trj_list.append(trj)
+            self.input_dict["x"].append(trj.x)
+            self.input_dict["y"].append(trj.y)
+            self.input_dict["vx"].append(trj.vx)
+            self.input_dict["vy"].append(trj.vy)
+            self.input_dict["flux"].append(trj.flux)
+            self.input_dict["likelihood"].append(trj.lh)
+            self.input_dict["obs_count"].append(trj.obs_count)
+
+    def _assert_results_match_dict(self, results, test_dict):
+        # Check that the shape of the results are the same.
+        self.assertEqual(len(results), len(test_dict["x"]))
+        self.assertEqual(set(results.colnames), set(test_dict.keys()))
+
+        for col in results.colnames:
+            for i in range(len(results)):
+                self.assertEqual(results[col][i], test_dict[col][i])
+
+    def test_create(self):
+        table = Results(self.trj_list)
+        self.assertEqual(len(table), self.num_entries)
+        self.assertEqual(len(table.colnames), 7)
+        self._assert_results_match_dict(table, self.input_dict)
+
+        # Test that we ignore invalid results, but track them in the filtered table.
+        self.trj_list[2].valid = False
+        self.trj_list[7].valid = False
+        table2 = Results(self.trj_list, track_filtered=True)
+        self.assertEqual(len(table2), self.num_entries - 2)
+        for i in range(self.num_entries - 2):
+            self.assertFalse(table2["x"][i] == 2 or table2["x"][i] == 7)
+
+        filtered = table2.get_filtered()
+        self.assertEqual(len(filtered), 2)
+        self.assertEqual(filtered["x"][0], 2)
+        self.assertEqual(filtered["x"][1], 7)
+
+    def test_from_dict(self):
+        self.input_dict["something_added"] = [i for i in range(self.num_entries)]
+
+        # Missing 'x' column
+        del self.input_dict["x"]
+        with self.assertRaises(KeyError):
+            _ = Results.from_dict(self.input_dict)
+
+        # Add back the 'x' column.
+        self.input_dict["x"] = [trj.x for trj in self.trj_list]
+        table = Results.from_dict(self.input_dict)
+        self._assert_results_match_dict(table, self.input_dict)
+
+    def test_from_table(self):
+        self.input_dict["something_added"] = [i for i in range(self.num_entries)]
+
+        # Missing 'x' column
+        del self.input_dict["x"]
+        with self.assertRaises(KeyError):
+            _ = Results.from_table(Table(self.input_dict))
+
+        # Add back the 'x' column.
+        self.input_dict["x"] = [trj.x for trj in self.trj_list]
+        table = Results.from_table(Table(self.input_dict))
+        self._assert_results_match_dict(table, self.input_dict)
+
+    def test_make_trajectory_list(self):
+        self.input_dict["something_added"] = [i for i in range(self.num_entries)]
+        table = Results.from_dict(self.input_dict)
+
+        trajectories = table.make_trajectory_list()
+        self.assertEqual(len(trajectories), self.num_entries)
+        for i, trj in enumerate(trajectories):
+            self.assertEqual(trj.x, table["x"][i])
+            self.assertEqual(trj.y, table["y"][i])
+            self.assertEqual(trj.vx, table["vx"][i])
+            self.assertEqual(trj.vy, table["vy"][i])
+            self.assertEqual(trj.obs_count, table["obs_count"][i])
+            self.assertEqual(trj.flux, table["flux"][i])
+            self.assertEqual(trj.lh, table["likelihood"][i])
+
+    def test_extend(self):
+        table1 = Results(self.trj_list)
+        for i in range(self.num_entries):
+            self.trj_list[i].x += self.num_entries
+        table2 = Results(self.trj_list)
+
+        table1.extend(table2)
+        self.assertEqual(len(table1), 2 * self.num_entries)
+        for i in range(2 * self.num_entries):
+            self.assertEqual(table1["x"][i], i)
+
+        # Fail with a mismatched table.
+        self.input_dict["something_added"] = [i for i in range(self.num_entries)]
+        table3 = Results.from_dict(self.input_dict)
+        with self.assertRaises(ValueError):
+            table1.extend(table3)
+
+    def test_add_psi_phi(self):
+        num_to_use = 3
+        table = Results(self.trj_list[0:num_to_use])
+        psi_array = np.array([[1.0, 1.1, 1.2, 1.3] for i in range(num_to_use)])
+        phi_array = np.array([[1.0, 1.0, 0.0, 2.0] for i in range(num_to_use)])
+        index_valid = np.array(
+            [
+                [True, True, True, True],
+                [True, False, True, True],
+                [False, False, False, False],
+            ]
+        )
+
+        exp_lh = [2.3, 2.020725, 0.0]
+        exp_flux = [1.15, 1.1666667, 0.0]
+        exp_obs = [4, 3, 0]
+
+        # Check the the data has been inserted and the statistics have been updated.
+        table.add_psi_phi_data(psi_array, phi_array, index_valid)
+        for i in range(num_to_use):
+            self.assertEqual(len(table["psi_curve"][i]), 4)
+            self.assertEqual(len(table["phi_curve"][i]), 4)
+            self.assertEqual(len(table["index_valid"][i]), 4)
+
+            self.assertAlmostEqual(table["likelihood"][i], exp_lh[i], delta=1e-5)
+            self.assertAlmostEqual(table["flux"][i], exp_flux[i], delta=1e-5)
+            self.assertEqual(table["obs_count"][i], exp_obs[i])
+
+    def test_filter_by_index(self):
+        table = Results(self.trj_list)
+        self.assertEqual(len(table), self.num_entries)
+
+        # Do the filtering and check we have the correct ones.
+        inds = [0, 2, 6, 7]
+        table.filter_by_index(inds)
+        self.assertEqual(len(table), len(inds))
+        for i in range(len(inds)):
+            self.assertEqual(table["x"][i], self.trj_list[inds[i]].x)
+
+        # Without tracking there should be nothing stored in the Results's
+        # filtered dictionary.
+        self.assertEqual(len(table.filtered), 0)
+        with self.assertRaises(ValueError):
+            table.get_filtered()
+
+        # Without tracking we cannot revert anything.
+        with self.assertRaises(ValueError):
+            table.revert_filter()
+
+    def test_filter_by_index_tracked(self):
+        table = Results(self.trj_list[0:10], track_filtered=True)
+        self.assertEqual(len(table), 10)
+
+        # Do the filtering. First remove elements 0 and 2. Then remove elements
+        # 0, 5, and 6 from the resulting list (1, 7, 8 in the original list).
+        table.filter_by_index([1, 3, 4, 5, 6, 7, 8, 9], label="filter1")
+        self.assertEqual(len(table), 8)
+        table.filter_by_index([1, 2, 3, 4, 7], label="filter2")
+        self.assertEqual(len(table), 5)
+        self.assertEqual(table["x"][0], 3)
+        self.assertEqual(table["x"][1], 4)
+        self.assertEqual(table["x"][2], 5)
+        self.assertEqual(table["x"][3], 6)
+        self.assertEqual(table["x"][4], 9)
+
+        # Check that we can get the correct filtered rows.
+        f1 = table.get_filtered("filter1")
+        self.assertEqual(len(f1), 2)
+        self.assertEqual(f1["x"][0], 0)
+        self.assertEqual(f1["x"][1], 2)
+
+        f2 = table.get_filtered("filter2")
+        self.assertEqual(len(f2), 3)
+        self.assertEqual(f2["x"][0], 1)
+        self.assertEqual(f2["x"][1], 7)
+        self.assertEqual(f2["x"][2], 8)
+
+        # Check that not passing a label gives us all filtered results.
+        f_all = table.get_filtered()
+        self.assertEqual(len(f_all), 5)
+
+        # Check that we can revert the filtering.
+        table.revert_filter("filter2")
+        self.assertEqual(len(table), 8)
+        expected_order = [3, 4, 5, 6, 9, 1, 7, 8]
+        for i, value in enumerate(expected_order):
+            self.assertEqual(table["x"][i], value)
+
+        # Check that we can revert the filtering and add a 'filtered_reason' column.
+        table = Results(self.trj_list[0:10], track_filtered=True)
+        table.filter_by_index([1, 3, 4, 5, 6, 7, 8, 9], label="filter1")
+        table.filter_by_index([1, 2, 3, 4, 7], label="filter2")
+        table.revert_filter(add_column="reason")
+        self.assertEqual(len(table), 10)
+        expected_order = [3, 4, 5, 6, 9, 0, 2, 1, 7, 8]
+        expected_reason = ["", "", "", "", "", "filter1", "filter1", "filter2", "filter2", "filter2"]
+        for i, value in enumerate(expected_order):
+            self.assertEqual(table["x"][i], value)
+            self.assertEqual(table["reason"][i], expected_reason[i])
+
+    def test_to_from_table_file(self):
+        max_save = 5
+        table = Results(self.trj_list[0:max_save], track_filtered=True)
+        table.table["other"] = [i for i in range(max_save)]
+        self.assertEqual(len(table), max_save)
+
+        # Test read/write to file.
+        with tempfile.TemporaryDirectory() as dir_name:
+            file_path = os.path.join(dir_name, "results.ecsv")
+            self.assertFalse(Path(file_path).is_file())
+
+            table.write_table(file_path)
+            self.assertTrue(Path(file_path).is_file())
+
+            table2 = Results.read_table(file_path)
+            self.assertEqual(len(table2), max_save)
+            self.assertTrue("other" in table2.colnames)
+            for col in ["x", "y", "vx", "vy", "likelihood", "flux", "obs_count", "other"]:
+                self.assertTrue(np.allclose(table[col], table2[col]))
+
+            # Cannot overwrite with it set to False
+            with self.assertRaises(OSError):
+                table.write_table(file_path, overwrite=False, cols_to_drop=["other"])
+
+            # We can overwrite with droped columns
+            table.write_table(file_path, overwrite=True, cols_to_drop=["other"])
+
+            table3 = Results.read_table(file_path)
+            self.assertEqual(len(table2), max_save)
+
+            # We only dropped the table from the save file.
+            self.assertFalse("other" in table3.colnames)
+            self.assertTrue("other" in table.colnames)
+
+
+if __name__ == "__main__":
+    unittest.main()
