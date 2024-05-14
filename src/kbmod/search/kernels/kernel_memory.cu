546f412a8d081b965482f0578d42a200ae90f881
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2024-05-14 12:41:20 -0400

    More informative GPU memory failure messages

--- src/kbmod/search/kernels/kernel_memory.cu
+++ src/kbmod/search/kernels/kernel_memory.cu
@@ -7,8 +7,9 @@
 #ifndef KERNELS_MEMORY_CU_
 #define KERNELS_MEMORY_CU_
 
-#include <stdexcept>
 #include <float.h>
+#include <string.h>
+#include <stdexcept>
 #include <vector>
 
 #include "cuda_errors.h"
@@ -21,28 +22,31 @@ namespace search {
 
 extern "C" void *allocate_gpu_block(unsigned long memory_size) {
     void *gpu_ptr;
-    checkCudaErrors(cudaMalloc((void **)&gpu_ptr, memory_size));
-    if (gpu_ptr == nullptr) throw std::runtime_error("Unable to allocate GPU memory.");
+    cudaError_t res = cudaMalloc((void **)&gpu_ptr, memory_size);
+    if (res != 0) throw std::runtime_error("Unable to allocate GPU memory. Error = " + std::to_string(res));
     return gpu_ptr;
 }
 
 extern "C" void free_gpu_block(void *gpu_ptr) {
     if (gpu_ptr == nullptr) throw std::runtime_error("Trying to free nullptr.");
-    checkCudaErrors(cudaFree(gpu_ptr));
+    cudaError_t res = cudaFree(gpu_ptr);
+    if (res != 0) throw std::runtime_error("Unable to free GPU memory. Error = " + std::to_string(res));
 }
 
 extern "C" void copy_block_to_gpu(void *cpu_ptr, void *gpu_ptr, unsigned long memory_size) {
     if (cpu_ptr == nullptr) throw std::runtime_error("Invalid CPU pointer");
     if (gpu_ptr == nullptr) throw std::runtime_error("Invalid GPU pointer");
 
-    checkCudaErrors(cudaMemcpy(gpu_ptr, cpu_ptr, memory_size, cudaMemcpyHostToDevice));
+    cudaError_t res = cudaMemcpy(gpu_ptr, cpu_ptr, memory_size, cudaMemcpyHostToDevice);
+    if (res != 0) throw std::runtime_error("Unable to copy data to GPU. Error = " + std::to_string(res));
 }
 
 extern "C" void copy_block_to_cpu(void *cpu_ptr, void *gpu_ptr, unsigned long memory_size) {
     if (cpu_ptr == nullptr) throw std::runtime_error("Invalid CPU pointer");
     if (gpu_ptr == nullptr) throw std::runtime_error("Invalid GPU pointer");
 
-    checkCudaErrors(cudaMemcpy(cpu_ptr, gpu_ptr, memory_size, cudaMemcpyDeviceToHost));
+    cudaError_t res = cudaMemcpy(cpu_ptr, gpu_ptr, memory_size, cudaMemcpyDeviceToHost);
+    if (res != 0) throw std::runtime_error("Unable to copy data to CPU. Error = " + std::to_string(res));
 }
 
 } /* namespace search */
