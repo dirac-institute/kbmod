091aca0679fd90ef144e5d503669437cb55e495f
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2024-10-18 10:48:09 -0400

    Move mjd_to_day to util_functions

--- src/kbmod/util_functions.py
+++ src/kbmod/util_functions.py
@@ -48,6 +48,22 @@ def get_matched_obstimes(obs_times, query_times, threshold=0.0007):
     return min_inds
 
 
+def mjd_to_day(mjd):
+    """Takes an mjd and converts it into a day in calendar date format.
+
+    Parameters
+    ----------
+    mjd : `float`
+        mjd format date.
+
+    Returns
+    ----------
+    A `str` with a calendar date, in the format YYYY-MM-DD.
+    e.g., mjd=60000 -> '2023-02-25'
+    """
+    return Time(mjd, format="mjd").strftime("%Y-%m-%d")
+
+
 def load_deccam_layered_image(filename, psf):
     """Load a layered image from the legacy deccam format.
 
--- tests/test_util_functions.py
+++ tests/test_util_functions.py
@@ -7,6 +7,7 @@ from kbmod.search import LayeredImage, PSF
 from kbmod.util_functions import (
     get_matched_obstimes,
     load_deccam_layered_image,
+    mjd_to_day,
 )
 
 
@@ -17,6 +18,11 @@ class test_util_functions(unittest.TestCase):
         matched_inds = get_matched_obstimes(obstimes, query_times, threshold=0.01)
         expected = [-1, 0, 0, -1, 1, 4, 5, 5, 7, 7, -1]
         self.assertTrue(np.array_equal(matched_inds, expected))
+
+    def test_mjd_to_day(self):
+        self.assertEqual(mjd_to_day(60481.04237269), "2024-06-20")
+        self.assertEqual(mjd_to_day(60500), "2024-07-09")
+        self.assertEqual(mjd_to_day(58100.5), "2017-12-13")
 
     def test_load_deccam_layered_image(self):
         base_path = Path(__file__).parent.parent
