cfd3c539e1102aa709f567243b71b417b70dd225
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-03-20 14:12:49 -0400

    Provide very basic analysis functionality with the fakes data

---
+++ src/kbmod/analysis/analyze_fakes.py
@@ -0,0 +1,225 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from kbmod.analysis.plotting import plot_image
+from kbmod.search import create_stamps, create_stamps_xy, Logging, Trajectory
+from kbmod.trajectory_utils import fit_trajectory_from_pixels, evaluate_trajectory_mse
+from kbmod.util_functions import get_matched_obstimes
+
+
+logger = Logging.getLogger(__name__)
+
+
+class FakeInfo:
+    """Information for a fake with bunch of helper functions.
+
+    Parameters
+    ----------
+    times : `np.ndarray`
+        The MJD times of the fakes.
+    ra : `np.ndarray`
+        The RA of the fakes at each time (in degrees).
+    dec : `np.ndarray`
+        The dec of the fakes at each time (in degrees).
+    name : `str`, optional
+        The name of the object.
+    image_inds : `np.ndarray`
+        For each observation of the fake the corresponding image index in the
+        WorkUnit's ImageStack.
+        Initially None until join_with_workunit() is called.
+    x_pos_fakes : `np.ndarray`
+        The x pixel position of the fakes.
+        Initially None until join_with_workunit() is called.
+    y_pos_fakes : `np.ndarray`
+        The y pixel position of the fakes.
+        Initially None until join_with_workunit() is called.
+    t_index_fakes : `np.ndarray`
+        An array of the indices that the fakes appear in the WorkUnit.
+        Initially None until join_with_workunit() is called.
+    trj : Trajectory
+        The best fitting linear trajectory from the pixel positions.
+        Initially None until join_with_workunit() is called.
+    xy_stamps : `np.ndarray`
+        A 3-d numpy array of stamps from the raw (x, y) values.
+        Initially None until join_with_workunit() is called.
+    trj_stamps : `np.ndarray`
+        A 3-d numpy array of stamps from the predicted locations of the
+        fitted trajectory.
+        Initially None until join_with_workunit() is called.
+    """
+
+    def __init__(self, times, ra, dec, name=None):
+        self.name = name
+        self.times = np.array(times)
+        self.ra = np.array(ra)
+        self.dec = np.array(dec)
+        self._validate_times()
+
+        self.image_inds = None
+        self.x_pos_fakes = None
+        self.y_pos_fakes = None
+        self.trj = None
+        self.xy_stamps = None
+        self.trj_stamps = None
+
+    def _validate_times(self):
+        """Sort the times and remove any duplicates."""
+        # Sort the data by time.
+        sorted_inds = np.argsort(self.times)
+        self.ra = self.ra[sorted_inds]
+        self.dec = self.dec[sorted_inds]
+        self.times = self.times[sorted_inds]
+
+        # Remove any duplicate times.
+        if np.any(np.diff(self.times) == 0.0):
+            dup_inds = np.where(np.diff(self.times) == 0.0)
+            self.times = np.delete(self.times, dup_inds)
+            self.ra = np.delete(self.ra, dup_inds)
+            self.dec = np.delete(self.dec, dup_inds)
+
+    def join_with_workunit(self, wu, radius=10):
+        """Compute and save as much auxiliary data as we can from a WorkUnit, including:
+            * The (x, y) of the fakes.
+            * The best fit linear trajectory from (x, y) values.
+
+        Parameters
+        ----------
+        wu : `WorkUnit`
+            The WorkUnit to use
+        radius : `int`
+            The stamp radius to use.  If <= 0 then skip stamp generation.
+        """
+        obstimes = wu.get_all_obstimes()
+        t0 = obstimes[0]
+
+        # Determine in which images the fake appears.
+        self.image_inds = get_matched_obstimes(obstimes, self.times, threshold=0.002)
+        if np.any(self.image_inds == -1):
+            raise ValueError(f"Unable to match one or more of the times:\n{obstimes}\n{self.times}")
+
+        # Compute the (x, y) pixel locations of the fakes in each image.
+        x_pos, y_pos = wu.get_pixel_coordinates(self.ra, self.dec, self.times)
+        self.x_pos_fakes = x_pos
+        self.y_pos_fakes = y_pos
+
+        # Fit a linear trajectory to the data.
+        zeroed_times = self.times - t0
+        self.trj = fit_trajectory_from_pixels(x_pos, y_pos, zeroed_times)
+
+        if radius > 0:
+            num_stamps = len(self.image_inds)
+
+            # Generate the stamps from the raw positions.
+            xy_stamp_list = create_stamps_xy(
+                wu.im_stack,
+                radius,
+                x_pos.astype(int),
+                y_pos.astype(int),
+                list(self.image_inds),
+            )
+
+            # Generate the stamps from the fitted trajectory.
+            trj_stamp_list = create_stamps_xy(
+                wu.im_stack,
+                radius,
+                (self.trj.x + self.trj.vx * zeroed_times + 0.5).astype(int),
+                (self.trj.y + self.trj.vy * zeroed_times + 0.5).astype(int),
+                list(self.image_inds),
+            )
+
+            # Transform to numpy arrays.
+            stamp_width = 2 * radius + 1
+            self.xy_stamps = np.zeros((num_stamps, stamp_width, stamp_width))
+            self.trj_stamps = np.zeros((num_stamps, stamp_width, stamp_width))
+            for i in range(num_stamps):
+                self.xy_stamps[i, :, :] = xy_stamp_list[i].image
+                self.trj_stamps[i, :, :] = trj_stamp_list[i].image
+
+    def compute_fit_mse(self):
+        """Compute the mean square error of the fitted trajectory."""
+        if self.trj is None:
+            raise ValueError("compute_fit_mse can only be called after join_with_workunit.")
+            
+        
+        return evaluate_trajectory_mse(
+            self.trj,
+            self.x_pos_fakes,
+            self.y_pos_fakes,
+            self.times - self.times[0],
+        )        
+
+    def compare_all_stamps(self):
+        """Plot a pair of raw (x, y) stamps and predicted location
+        stamps for a given time step.
+
+        Note
+        ----
+        Must be called after join_with_workunit().
+
+        Parameters
+        ----------
+        index : `int`
+            The time index of the stamps to plot.
+        figure : `matplotlib.pyplot.Figure` or `None`
+            Figure, `None` by default.
+        """
+        if self.xy_stamps is None or self.trj_stamps is None:
+            raise ValueError("plot_stamps can only be called after join_with_workunit.")
+
+        num_stamps = len(self.xy_stamps)
+        fig, axes = plt.subplots(num_stamps, 2, figsize=(3.0 * 2, 3.0 * num_stamps))
+        fig.tight_layout()
+
+        for i in range(num_stamps):
+            plot_image(
+                self.xy_stamps[i, :, :],
+                ax=axes[i, 0],
+                figure=fig,
+                norm=True,
+                title=f"Fake Given Pos\n({self.times[i]})",
+                show_counts=False,
+            )
+            plot_image(
+                self.trj_stamps[i, :, :],
+                ax=axes[i, 1],
+                figure=fig,
+                norm=True,
+                title=f"Trj Predicted Pos\n({self.times[i]})",
+                show_counts=False,
+            )
+        plt.show()
+
+
+def load_fake_info_from_ecsv(filename, time_adjust=0.00112558):
+    """Load the info for all the fakes from an ecsv file.
+
+    Parameters
+    ----------
+    filename : `str`
+        The name of the file with the fake data.
+    time_adjust : `float`
+        The difference between the fake's mjd_mid and the image
+        time stamp (start of the image).
+
+    Returns
+    -------
+    fakes : `list`
+        A list of FakeInfo objects.
+    """
+    import pandas as pd
+
+    logger.info(f"Loading fakes from {filename}")
+    fakes_df = pd.read_csv(filename, comment="#", header=0, sep=" ")
+    logger.info(f"Loaded the fakes file with {len(fakes_df)} rows.")
+
+    obj_ids = np.unique(fakes_df["ORBITID"])
+    logger.info(f"Found {len(obj_ids)} unique objects.")
+
+    fakes = []
+    for obj in obj_ids:
+        row_mask = fakes_df["ORBITID"] == obj
+        ra = fakes_df["RA"][row_mask].values
+        dec = fakes_df["DEC"][row_mask].values
+        times = fakes_df["mjd_mid"][row_mask].values + time_adjust
+        fakes.append(FakeInfo(times, ra, dec, name=obj))
+    return fakes
