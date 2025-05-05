333dd9ee4cb25d8deefe8a09bb5266c0f0f1a19e
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-05-05 10:20:26 -0400

    Enable filtering and append for the Python ImageStack

--- tests/test_image_stack_py.py
+++ tests/test_image_stack_py.py
@@ -57,11 +57,56 @@ class test_image_stack_py(unittest.TestCase):
             self.assertTrue(np.all(stack.var[idx] == 0.1))
         self.assertEqual(len(stack.psfs), num_times)
 
+        # We can append an image and a LayeredImage.
+        stack.append_image(0, sci[0], var[0])
+        self.assertEqual(stack.num_times, num_times + 1)
+        self.assertEqual(len(stack.sci), num_times + 1)
+        self.assertEqual(len(stack.var), num_times + 1)
+        self.assertEqual(len(stack.psfs), num_times + 1)
+        self.assertTrue(np.allclose(stack.times, np.append(times, 0.0)))
+
+        layered = LayeredImagePy(sci[1], var[1], time=5.0)
+        stack.append_layered_image(layered)
+        self.assertEqual(stack.num_times, num_times + 2)
+        self.assertEqual(len(stack.sci), num_times + 2)
+        self.assertEqual(len(stack.var), num_times + 2)
+        self.assertEqual(len(stack.psfs), num_times + 2)
+        self.assertEqual(stack.times[num_times + 1], 5.0)
+
         # Test that we fail with bad input.
-        self.assertRaises(ValueError, ImageStackPy, np.array([]), sci, var)
-        self.assertRaises(ValueError, ImageStackPy, None, sci, var)
+        self.assertRaises(ValueError, ImageStackPy, times)
         self.assertRaises(ValueError, ImageStackPy, times, [np.array([1.0])], var)
         self.assertRaises(ValueError, ImageStackPy, times, sci, [np.array([1.0])])
+
+    def test_create_empty_image_stack_py(self):
+        """Test that we can create an empty ImageStackPy"""
+        stack = ImageStackPy()
+        self.assertEqual(stack.num_times, 0)
+        self.assertEqual(stack.width, -1)
+        self.assertEqual(stack.height, -1)
+
+        # We can append the first few images.
+        num_times = 5
+        height = 20
+        width = 15
+        for idx in range(num_times):
+            sci = np.full((height, width), float(idx))
+            var = np.full((height, width), 0.1 * float(idx))
+            stack.append_image(float(idx + 5.0), sci, var)
+
+            self.assertEqual(stack.num_times, idx + 1)
+            self.assertEqual(stack.width, width)
+            self.assertEqual(stack.height, height)
+            self.assertTrue(np.allclose(stack.sci[idx], sci))
+            self.assertTrue(np.allclose(stack.var[idx], var))
+        self.assertTrue(np.allclose(stack.times, [5.0, 6.0, 7.0, 8.0, 9.0]))
+        self.assertTrue(np.allclose(stack.zeroed_times, [0.0, 1.0, 2.0, 3.0, 4.0]))
+
+        # We fail if we try to create an ImageStackPy with no times but other data.
+        times = np.arange(num_times)
+        sci = [np.full((height, width), float(i)) for i in range(num_times)]
+        var = [np.full((height, width), 0.1 * i) for i in range(num_times)]
+        self.assertRaises(ValueError, ImageStackPy, None, sci, var)
 
     def test_create_image_stack_from_nparray_py(self):
         """Test that we can create an ImageStackPy from a single 3-d numpy array."""
@@ -161,6 +206,36 @@ class test_image_stack_py(unittest.TestCase):
         self.assertEqual(stack.times[5], 10.0)
         self.assertEqual(stack.zeroed_times[5], 10.0)
 
+    def test_filter_image_stack_ph(self):
+        """Test that we can filter an ImageStackPy"""
+        num_times = 10
+        height = 20
+        width = 15
+
+        times = np.arange(num_times) + 3.0
+        sci = [np.full((height, width), float(i)) for i in range(num_times)]
+        var = [np.full((height, width), 0.1 * float(i)) for i in range(num_times)]
+        psfs = [np.array([[float(i)]]) for i in range(num_times)]
+
+        stack = ImageStackPy(times, sci, var, psfs=psfs)
+        self.assertEqual(stack.num_times, num_times)
+
+        keep_inds = np.array([1, 3, 5, 6, 7, 8])
+        keep_mask = np.full(num_times, False)
+        keep_mask[keep_inds] = True
+        stack.filter_images(keep_mask)
+        self.assertEqual(stack.num_times, len(keep_inds))
+
+        # Check that we saved the correct indices.
+        for new_idx, org_idx in enumerate(keep_inds):
+            self.assertTrue(np.allclose(stack.sci[new_idx], sci[org_idx]))
+            self.assertTrue(np.allclose(stack.var[new_idx], var[org_idx]))
+            self.assertTrue(np.allclose(stack.psfs[new_idx], psfs[org_idx]))
+
+        # Check the times. Note the zeroed times shift because we removed index 0.
+        self.assertTrue(np.allclose(stack.times, [4.0, 6.0, 8.0, 9.0, 10.0, 11.0]))
+        self.assertTrue(np.allclose(stack.zeroed_times, [0.0, 2.0, 4.0, 5.0, 6.0, 7.0]))
+
     def test_get_matched_obstimes(self):
         obstimes = [1.0, 2.0, 3.0, 4.0, 6.0, 7.5, 9.0, 10.1]
 
@@ -219,6 +294,30 @@ class test_image_stack_py(unittest.TestCase):
 
             # Far away from the object, the signal should be zero.
             self.assertAlmostEqual(fake_stack.sci[t_idx][30, 40], 0.0)
+
+    def test_get_masked_fractions(self):
+        """Test that we can compute the fraction of pixels masked at each index."""
+        num_times = 20
+        height = 20
+        width = 30
+        stack = ImageStackPy()
+
+        # Append images with an increasing fraction of masked pixels
+        for idx in range(num_times):
+            sci = np.full((height, width), float(idx))
+            var = np.full((height, width), 0.1 * float(idx))
+            msk = np.zeros((height, width))
+            msk[0:idx, :] = 1
+            stack.append_image(idx / 20.0, sci, var, mask=msk)
+
+        fractions = stack.get_masked_fractions()
+        self.assertTrue(np.allclose(fractions, np.arange(num_times) / float(num_times)))
+
+        # Filter out images with above a given masked fraction.
+        stack.filter_images(fractions < 0.5)
+        times_remaining = int(num_times / 2)
+        self.assertEqual(stack.num_times, times_remaining)
+        self.assertTrue(np.allclose(stack.times, np.arange(times_remaining) / float(num_times)))
 
     def test_image_stack_py_validate(self):
         """Tests that we can validate an ImageStackPy."""
