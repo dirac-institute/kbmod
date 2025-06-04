b110594077b5c16f7395449c4e876fa635606fdc
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-06-04 13:26:21 -0400

    Combine functions

--- src/kbmod/filters/brightness_filters.py
+++ src/kbmod/filters/brightness_filters.py
@@ -1,10 +1,11 @@
 import numpy as np
+import torch
 
 from kbmod.core.stamp_utils import extract_curve_values
 
 
-def extract_sci_curves(res, stack, append=True):
-    """Extract the science pixel curves for each candidate in the results object.
+def extract_sci_var_curves(res, stack, keep_nans=True, append=True):
+    """Extract the science and variance pixel curves for each candidate in the results object.
 
     Parameters
     ----------
@@ -12,46 +13,34 @@ def extract_sci_curves(res, stack, append=True):
         The search results object containing trajectories.
     stack : `ImageStack`
         The image stack used for the search, containing the images.
+    keep_nans : `bool`, optional
+        If True, the extracted curves will keep NaN values for masked pixels (including out of bounds).
+        If False, those values will be replaced with 0 for science and 1 for variance.
     append : `bool`, optional
         If True, the extracted curves will be appended to the results data.
+        Default: True
 
     Returns
     -------
     sci_curves : `numpy.ndarray`
         An array of shape (num_candidates, num_times) containing the extracted science pixel values
-        with NaN for pixels outside the image bounds.
+        with the mask value (default=NaN) for pixels outside the image bounds.
     """
+    # Compute the predicted x and y positions for each candidate at each time step.
+    # The +0.5 is to account for the pixel center offset.
     x_pos = (res["x"][:, np.newaxis] + stack.zeroed_times * res["vx"][:, np.newaxis] + 0.5).astype(int)
     y_pos = (res["y"][:, np.newaxis] + stack.zeroed_times * res["vy"][:, np.newaxis] + 0.5).astype(int)
 
+    # Extract the science and variance pixels values and replace the masked values if needed.
     sci_curves = extract_curve_values(stack.sci, x_pos, y_pos)
-    if append:
-        res.table["sci_curves"] = sci_curves
-    return sci_curves
-
-
-def extract_var_curves(res, stack, append=True):
-    """Extract the variance pixel curves for each candidate in the results object.
-
-    Parameters
-    ----------
-    res : `Results``
-        The search results object containing trajectories.
-    stack : `ImageStack`
-        The image stack used for the search, containing the images.
-    append : `bool`, optional
-        If True, the extracted curves will be appended to the results data.
-
-    Returns
-    -------
-    var_curves : `numpy.ndarray`
-        An array of shape (num_candidates, num_times) containing the extracted variance pixel values
-        with NaN for pixels outside the image bounds.
-    """
-    x_pos = (res["x"][:, np.newaxis] + stack.zeroed_times * res["vx"][:, np.newaxis] + 0.5).astype(int)
-    y_pos = (res["y"][:, np.newaxis] + stack.zeroed_times * res["vy"][:, np.newaxis] + 0.5).astype(int)
-
     var_curves = extract_curve_values(stack.var, x_pos, y_pos)
+    if not keep_nans:
+        sci_curves[np.isnan(sci_curves)] = 0
+        var_curves[np.isnan(var_curves)] = 1
+
+    # Append to extracted curves to the results.
     if append:
-        res.table["var_curves"] = var_curves
-    return var_curves
+        res.table["sci_curve"] = sci_curves
+        res.table["var_curve"] = var_curves
+
+    return sci_curves, var_curves
--- tests/test_brightness_filters.py
+++ tests/test_brightness_filters.py
@@ -3,10 +3,7 @@ import unittest
 import numpy as np
 
 from kbmod.core.image_stack_py import ImageStackPy
-from kbmod.filters.brightness_filters import (
-    extract_sci_curves,
-    extract_var_curves,
-)
+from kbmod.filters.brightness_filters import extract_sci_var_curves
 from kbmod.results import Results
 from kbmod.search import Trajectory
 
@@ -24,18 +21,29 @@ class TestBrightnessFilters(unittest.TestCase):
 
         trjs = [Trajectory(x=0, y=0, vx=1, vy=1), Trajectory(x=1, y=3, vx=1, vy=2)]
         results = Results.from_trajectories(trjs, track_filtered=False)
+        self.assertFalse("sci_curve" in results.colnames)
+        self.assertFalse("var_curve" in results.colnames)
 
-        sci_curves = extract_sci_curves(results, im_stack, append=True)
-        expected_sci_curves = [[0, 36, 72, 108], [16, 57, np.nan, np.nan]]
-        self.assertTrue("sci_curves" in results.colnames)
-        self.assertEqual(sci_curves.shape, (len(trjs), num_times))
+        expected_sci_curves = np.array([[0, 36, 72, 108], [16, 57, np.nan, np.nan]])
+        expected_var_curves = np.array([[0, 3.6, 7.2, 10.8], [1.6, 5.7, np.nan, np.nan]])
+
+        sci_curves, var_curves = extract_sci_var_curves(results, im_stack, append=False)
+        self.assertFalse("sci_curve" in results.colnames)
+        self.assertFalse("var_curve" in results.colnames)
         self.assertTrue(np.allclose(sci_curves, expected_sci_curves, equal_nan=True))
-
-        var_curves = extract_var_curves(results, im_stack, append=True)
-        expected_var_curves = [[0, 3.6, 7.2, 10.8], [1.6, 5.7, np.nan, np.nan]]
-        self.assertTrue("var_curves" in results.colnames)
-        self.assertEqual(var_curves.shape, (len(trjs), num_times))
         self.assertTrue(np.allclose(var_curves, expected_var_curves, equal_nan=True))
+
+        _, _ = extract_sci_var_curves(results, im_stack, append=True)
+        self.assertTrue("sci_curve" in results.colnames)
+        self.assertTrue("var_curve" in results.colnames)
+        self.assertTrue(np.allclose(results["sci_curve"], expected_sci_curves, equal_nan=True))
+        self.assertTrue(np.allclose(results["var_curve"], expected_var_curves, equal_nan=True))
+
+        sci_curves, var_curves = extract_sci_var_curves(results, im_stack, keep_nans=False, append=False)
+        expected_sci_curves2 = np.array([[0, 36, 72, 108], [16, 57, 0.0, 0.0]])
+        expected_var_curves2 = np.array([[0, 3.6, 7.2, 10.8], [1.6, 5.7, 1.0, 1.0]])
+        self.assertTrue(np.allclose(sci_curves, expected_sci_curves2, equal_nan=True))
+        self.assertTrue(np.allclose(var_curves, expected_var_curves2, equal_nan=True))
 
 
 if __name__ == "__main__":
