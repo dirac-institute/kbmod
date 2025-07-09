c978cce7cbbbbd0d3fbcf028b74bfb56afb851c1
SamSandwich07 <135195778+SamSandwich07@users.noreply.github.com>
2025-07-09 13:33:59 -0700

    SNS Filter Scaffolding (#1029)
    * Initial commit
    
    * updated formatting
    
    * addressed initial code review; updates no_op_test and added docstring
    
    * updated formatting
    
    ---------
    
    Co-authored-by: Samarth Ramya Venkatesh <samnsid7@gondor.astro.washington.edu>

---
+++ src/kbmod/filters/sns_filters.py
@@ -0,0 +1,12 @@
+"""KBMOD Implementation of Wes Fraser's SNS filtering. Done as part of @SamSandwich07's summer project"""
+
+
+def no_op_filter(res):
+    """This is a no op function to be removed at a later date
+
+    Parameters
+    ----------
+    res : `Results`
+        the KBMOD results object
+
+    """
---
+++ tests/test_sns_filter.py
@@ -0,0 +1,28 @@
+import unittest
+import numpy as np
+from kbmod.results import Results
+from kbmod.fake_data.fake_data_creator import FakeDataSet
+from kbmod.filters.sns_filters import no_op_filter
+
+
+class TestSnsFilter(unittest.TestCase):
+    def test_no_op_does_not_modify_results(self):
+        num_times = 10
+        height = 40
+        width = 50
+        times = np.arange(num_times)
+
+        # Create a fake data set a few fake objects with different fluxes.
+        ds = FakeDataSet(width=width, height=height, times=times, use_seed=11, psf_val=1e-6)
+        ds.insert_random_object(flux=5)
+        ds.insert_random_object(flux=20)
+        ds.insert_random_object(flux=50)
+        results = Results.from_trajectories(ds.trajectories, track_filtered=False)
+        length = len(results)
+
+        no_op_filter(results)
+        self.assertEqual(length, len(results))
+
+
+if __name__ == "__main__":
+    unittest.main()
