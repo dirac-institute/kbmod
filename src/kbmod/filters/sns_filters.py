f537e65f9b3bf3f12186a6a8706e913150e1c5ac
SamSandwich07 <135195778+SamSandwich07@users.noreply.github.com>
2025-08-15 14:53:29 -0700

    Issue/1040 (#1042)
    * peak_offset_filter separated into its own flag
    
    * scaffolding for predictive_line_cluster implemented
    
    * draft predictive_line_clustering fully implemented
    
    * Unit test implemented
    
    * Unit test implemented
    
    * extraneous/unused code and comments deleted
    
    * extraneous/unused code and comments deleted
    
    * code review issues addressed
    
    * deleted unused import
    
    * code review issues addressed
    
    * code review issues addressed
    
    * updated some test case comments
    
    * snr calculation now uses sqrt
    
    * updated notebooks folder
    
    * notebooks output cleared, predictive_line_cluster value of 55 updated
    
    * formatting updated
    
    * added additional notebook
    
    * initial documentation update to include peak_offset_max (#1049)
    
    * added argument descriptions for predictive_line_cluster
    
    * documentation updated for predictive_line_cluster
    
    * Add support for target fields to kbmod_build_ic (#1039)
    
    * Add support for target fields to kbmod_build_ic
    
    * Lint
    
    * separate `image_positions_to_original_icrs` from `WorkUnit` (#1044)
    
    * initial swap out
    
    * further clean up and black formatting
    
    * Fix for creating image stacks with variably sized images on latest butler (#1045)
    
    * Support ingesting chained collections
    
    * update bulter standardizer, add pixel_scale field, allow different image sizes in stack
    
    * Update mock butler
    
    * Test stack size changes
    
    * linting
    
    * Update comments
    
    * add torch version constraint (#1051)
    
    ---------
    
    Co-authored-by: Wilson Beebe <wbeebe@uw.edu>
    Co-authored-by: Max West <110124344+maxwest-uw@users.noreply.github.com>

--- src/kbmod/filters/sns_filters.py
+++ src/kbmod/filters/sns_filters.py
@@ -1,4 +1,5 @@
 import numpy as np
+from kbmod.results import Results
 
 """KBMOD Implementation of Wes Fraser's SNS filtering.
 Done as part of @SamSandwich07's summer project"""
@@ -36,3 +37,102 @@ def peak_offset_filter(res, peak_offset_max=6):
     radial_d = ((X - b / 2) ** 2 + (Y - a / 2) ** 2) ** 0.5
     w = np.where(radial_d < peak_offset_max)
     res.table = res.table[w]
+
+
+def predictive_line_cluster(res, dmjds, dist_lim=4.0, min_samp=2, init_select_proc_distance=60):
+    """Determine clustering based on the linearity of the best centroid.
+
+    Parameters
+    ----------
+    res : `Results`
+        The search results containing trajectories.
+        This object is modified by filtering out rows.
+    dmjds : `list`
+        List of dates in dmjd format.
+    dist_lim : `double`
+        Distance limits for final clustering.
+    min_samp : `int`
+        Minimum number of trajectories clustered to be included in final result.
+    init_select_proc_distance : `int`
+        Radius range through x, y and vx, vy space to search for clusters.
+        Mainly used for memory management.
+    """
+
+    snr = res["psi_curve"] / np.sqrt(res["phi_curve"])
+    max_snr = np.max(snr, axis=1)
+
+    x_col = res["x"].data
+    y_col = res["y"].data
+    vx_col = res["vx"].data
+    vy_col = res["vy"].data
+    res_copy = res
+
+    proc_inds = np.arange(len(x_col))
+    clust_inds = []
+
+    while len(max_snr) > 0:
+        arg_max = np.argmax(max_snr)
+        x_o = x_col[arg_max]
+        y_o = y_col[arg_max]
+        rx_o = vx_col[arg_max]
+        ry_o = vy_col[arg_max]
+
+        # this secondary where command is necessary because of memory overflows in large detection lists
+        w = np.where(
+            (x_col > x_o - init_select_proc_distance)
+            & (x_col < x_o + init_select_proc_distance)
+            & (y_col > y_o - init_select_proc_distance)
+            & (y_col < y_o + init_select_proc_distance)
+        )
+
+        W = np.where(
+            ((x_col[w[0]] - x_col[arg_max]) ** 2 + (y_col[w[0]] - y_col[arg_max]) ** 2)
+            < init_select_proc_distance**2
+        )
+
+        w = w[0][W[0]]
+
+        x_col_subset = x_col[w]
+        y_col_subset = y_col[w]
+        vx_col_subset = vx_col[w]
+        vy_col_subset = vy_col[w]
+
+        drx = vx_col_subset - rx_o
+        dry = vy_col_subset - ry_o
+        dt = dmjds  # just for clarity
+
+        x_n, y_n = (
+            x_o - drx * dt[-1],
+            y_o - dry * dt[-1],
+        )  # predicted centroid  position of secondary detection shifted at the differential wrong rate.
+
+        dx, dy = (x_n - x_o), (
+            y_n - y_o
+        )  # predicted centroid shifted such that best detection is now at origin
+        dxp = dx * y_col_subset
+        dyp = dy * x_col_subset
+        xm = x_n * y_o
+        ym = y_n * x_o
+        dx2 = dx**2
+        dy2 = dy**2
+        top = np.abs(dyp - dxp + xm - ym)
+        bottom = np.sqrt(dx2 + dy2)
+        dist = top / bottom
+
+        clust = np.where((dist < dist_lim) | (np.isnan(dist)) | ((dist < dist_lim) & (drx == 0) & (dry == 0)))
+
+        if len(clust[0]) >= min_samp:
+            clust_inds.append(proc_inds[arg_max])
+
+        mask = np.ones(len(res_copy), dtype="bool")
+        mask[w[clust]] = False
+        x_col = x_col[mask]
+        y_col = y_col[mask]
+        vx_col = vx_col[mask]
+        vy_col = vy_col[mask]
+        res_copy = res_copy[mask]
+        proc_inds = proc_inds[mask]
+        max_snr = max_snr[mask]
+
+    clust_inds.sort()
+    res.table = res.table[clust_inds]
---
+++ tests/test_sns_filters.py
@@ -0,0 +1,105 @@
+import unittest
+import numpy as np
+from kbmod.results import Results
+from kbmod.fake_data.fake_data_creator import FakeDataSet
+from kbmod.configuration import SearchConfiguration
+from kbmod.search import Trajectory
+from kbmod.filters.sns_filters import *
+
+
+class TestSnsFilter(unittest.TestCase):
+    def test_peak_offset_filter_throws_exception(self):
+        # empty results raises runtime exception since there is not "coadd_mean" column
+        empty_results = Results()
+        self.assertRaises(RuntimeError, peak_offset_filter, empty_results)
+
+    def test_peak_offset_filter(self):
+        num_times = 10
+        height = 40
+        width = 50
+        times = np.arange(num_times) + 60676  # MJD for Jan 1, 2025
+
+        # Create a fake data set a few fake objects with different fluxes.
+        ds = FakeDataSet(width=width, height=height, times=times, use_seed=11, psf_val=1e-6)
+        for itr in np.arange(5):
+            ds.insert_random_object(flux=5 * itr)
+            itr += 1
+
+        results = ds.make_results()
+        self.assertTrue("coadd_mean" in results.colnames)
+
+        # Ensure that a peak_offset_max of 0 filters everything out
+        peak_offset_filter(results, peak_offset_max=0)
+        self.assertEqual(0, len(results))
+
+        # Ensure that a peak_offset_max of 10,000 filters nothing out
+        results = ds.make_results()
+        peak_offset_filter(results, peak_offset_max=10000)
+        self.assertEqual(5, len(results))
+
+        # Insert a sixth object and edit it to be outside of a peak offset of 1.
+        # Two objects should be filtered out:
+        #   - one with a flux of zero
+        #   - one outside of the peak_offset
+        trj = ds.insert_random_object(flux=25)
+        trj.x = trj.x - 2 if trj.x >= 38 else trj.x + 2
+        results = ds.make_results()
+        peak_offset_filter(results, peak_offset_max=1)
+        self.assertEqual(4, len(results))
+
+    def test_predictive_line_cluster(self):
+        num_times = 10
+        height = 40
+        width = 50
+        times = np.arange(num_times) + 60676  # MJD for Jan 1, 2025
+
+        # Create a fake data set a few fake objects with different fluxes.
+        ds1 = FakeDataSet(width=width, height=height, times=times, use_seed=11, psf_val=1e-6)
+
+        trj = ds1.insert_random_object(5)
+        for it in np.arange(5):
+            ds1.trajectories.append(trj)
+        results = ds1.make_results()
+
+        self.assertEqual(6, len(results))  # 6 total trajectories inserted
+        predictive_line_cluster(results, times)
+        # All 6 trajectories are the same so expect them to be clustered into 1
+        self.assertEqual(1, len(results))
+
+        # Insert 5 trajectories slightly perturbed from each other
+        ds2 = FakeDataSet(width=width, height=height, times=times, use_seed=11, psf_val=1e-6)
+        trj = ds2.insert_random_object(5)
+
+        trj1 = Trajectory()
+        trj1.x = trj.x + 1
+        trj1.y = trj.y + 1
+
+        trj2 = Trajectory()
+        trj2.x = trj.x + 1
+        trj2.y = trj.y - 1
+
+        trj3 = Trajectory()
+        trj3.x = trj.x - 1
+        trj3.x = trj.x + 1
+
+        trj4 = Trajectory()
+        trj4.x = trj.x - 1
+        trj4.x = trj.x - 1
+
+        ds2.trajectories.append(trj1)
+        ds2.trajectories.append(trj2)
+        ds2.trajectories.append(trj3)
+        ds2.trajectories.append(trj4)
+        results = ds2.make_results()
+        self.assertEqual(5, len(results))  # 5 total trajectories inserted
+        predictive_line_cluster(results, times)
+
+        # All 5 trajectories are close together so expect them to be clustered into 1
+        self.assertEqual(1, len(results))
+        # The remaining trajectory should match the first trajectory inserted
+        self.assertEqual(trj.x, results["x"][0])
+        self.assertEqual(trj.y, results["y"][0])
+
+
+if __name__ == "__main__":
+    unittest.main()
