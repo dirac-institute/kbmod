f8e51fd1a9ed399fd6ee4163ff3d4c835c4d42a0
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2026-01-30 12:49:52 -0500

    Update kernels.cu

--- src/kbmod/search/kernels/kernels.cu
+++ src/kbmod/search/kernels/kernels.cu
@@ -256,6 +256,15 @@ __global__ void searchFilterImages(PsiPhiArrayMeta psi_phi_meta, void *psi_phi_v
     assert(psi_phi_vect != nullptr && image_times != nullptr && trajectories != nullptr &&
            results != nullptr);
 
+    // copy their time before progressing. We need to do this before pruning on
+    // (x, y) in order to correctly handle blocks at the edge of the image.
+    __shared__ double shared_times[MAX_NUM_IMAGES];
+    int time_idx = threadIdx.x + threadIdx.y * blockDim.x;
+    if ((time_idx < psi_phi_meta.num_times) && (time_idx < MAX_NUM_IMAGES)) {
+        shared_times[time_idx] = image_times[time_idx];
+    }
+    __syncthreads();  // Block until all are done loading.
+
     // Get the x and y coordinates within the search space.
     const int x_i = blockIdx.x * THREAD_DIM_X + threadIdx.x;
     const int y_i = blockIdx.y * THREAD_DIM_Y + threadIdx.y;
@@ -301,7 +310,7 @@ __global__ void searchFilterImages(PsiPhiArrayMeta psi_phi_meta, void *psi_phi_v
         curr_trj.obs_count = 0;
 
         // Evaluate the trajectory.
-        evaluateTrajectory(psi_phi_meta, psi_phi_vect, image_times, params, &curr_trj);
+        evaluateTrajectory(psi_phi_meta, psi_phi_vect, shared_times, params, &curr_trj);
 
         // If we do not have enough observations or a good enough LH score,
         // do not bother inserting it into the sorted list of results.
