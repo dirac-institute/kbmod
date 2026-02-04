c2a76abb6fd348fad00832c99c145b9aa8289d1c
Wilson Beebe <wbeebe@uw.edu>
2026-02-03 16:22:26 -0800

    Always skip reflex-correcting a guess distance of 0

--- src/kbmod/image_collection.py
+++ src/kbmod/image_collection.py
@@ -562,6 +562,9 @@ class ImageCollection:
         """
         guess_dists = [guess_distance] if not isinstance(guess_distance, list) else guess_distance
         for guess_dist in guess_dists:
+            if guess_dist == 0.0:
+                # Skip correction for distance 0.0. `reflex_corrected_col` will return the original column name.
+                continue
             # Calculate the parallax correction for each RA, Dec in the ImageCollection
             corrected_ra_dec, _ = correct_parallax_geometrically_vectorized(
                 self.data["ra"],
